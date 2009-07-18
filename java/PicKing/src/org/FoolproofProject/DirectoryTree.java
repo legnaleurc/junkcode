78c38212cb89f92601e0de66019d3aea3ae892b1
legnaleurc <legnaleurc@gmail.com>
2009-07-18 14:30:19 +0800

    [Source] Change file tree to dir tree.

--- java/PicKing/src/org/FoolproofProject/DirectoryTree.java
+++ java/PicKing/src/org/FoolproofProject/DirectoryTree.java
@@ -1,40 +1,55 @@
 package org.FoolproofProject;
 
 import java.io.File;
+import java.io.FileFilter;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.event.TreeModelListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.TreeModel;
 import javax.swing.tree.TreePath;
 
-public class FileTree extends JPanel {
+public class DirectoryTree extends JPanel {
 	
 	private static final long serialVersionUID = -8724999594568776949L;
 	
-	public FileTree() {
-		this( "." );
+	public DirectoryTree() {
+		this( File.listRoots()[0] );
 	}
 	
-	public FileTree( String path ) {
+	public DirectoryTree( String path ) {
 		this( new File( path ) );
 	}
 	
-	public FileTree( File root ) {
-		FileTreeModel model = new FileTreeModel( root );
+	public DirectoryTree( File root ) {
+		DirectoryTreeModel model = new DirectoryTreeModel( root );
 		JTree view = new JTree( model );
 		JScrollPane scroll = new JScrollPane( view );
 		setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );
 		add( scroll );
+		
+		view.addTreeSelectionListener( new TreeSelectionListener() {
+			@Override
+			public void valueChanged(TreeSelectionEvent e) {
+				TreePath[] paths = ( ( JTree )e.getSource() ).getSelectionPaths();
+				System.err.println( "<?" );
+				for( TreePath path : paths ) {
+					System.err.println( path.getLastPathComponent() );
+				}
+				System.err.println( "?>" );
+			}
+		} );
 	}
 	
-	private class FileTreeModel implements TreeModel {
+	private class DirectoryTreeModel implements TreeModel {
 		
 		private File root;
 		
-		public FileTreeModel( File root ) {
+		public DirectoryTreeModel( File root ) {
 			this.root = root;
 		}
 
@@ -46,16 +61,16 @@ public class FileTree extends JPanel {
 
 		@Override
 		public File getChild(Object parent, int index) {
-			String[] children = ( ( File )parent ).list();
+			File[] children = ( ( File )parent ).listFiles( new DirectoryFilter() );
 			if( children == null || ( index >= children.length ) ) {
 				return null;
 			}
-			return new File( ( File )parent, children[index] );
+			return new File( ( File )parent, children[index].getName() );
 		}
 
 		@Override
 		public int getChildCount(Object parent) {
-			String[] children = ( ( File )parent ).list();
+			File[] children = ( ( File )parent ).listFiles( new DirectoryFilter() );
 			if( children == null ) {
 				return 0;
 			}
@@ -64,7 +79,7 @@ public class FileTree extends JPanel {
 
 		@Override
 		public int getIndexOfChild(Object parent, Object child) {
-			String[] children = ( ( File )parent ).list();
+			File[] children = ( ( File )parent ).listFiles( new DirectoryFilter() );
 			if( children == null ) {
 				return -1;
 			}
@@ -84,7 +99,7 @@ public class FileTree extends JPanel {
 
 		@Override
 		public boolean isLeaf(Object node) {
-			return ( ( File )node ).isFile();
+			return ( ( File )node ).listFiles( new DirectoryFilter() ) == null;
 		}
 
 		@Override
@@ -97,6 +112,13 @@ public class FileTree extends JPanel {
 		public void valueForPathChanged(TreePath path, Object newValue) {
 			// TODO Auto-generated method stub
 			
+		}
+		
+		private class DirectoryFilter implements FileFilter {
+			@Override
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
 		}
 	}
 
--- java/PicKing/src/org/FoolproofProject/Main.java
+++ java/PicKing/src/org/FoolproofProject/Main.java
@@ -20,7 +20,7 @@ public class Main {
 				window.setLayout( new BoxLayout( window.getContentPane(), BoxLayout.Y_AXIS ) );
 				window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 				window.setSize( 640, 480 );
-				window.getContentPane().add( new FileTree() );
+				window.getContentPane().add( new DirectoryTree() );
 				window.setVisible(true);
 			}
 		} );
