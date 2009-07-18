4ff62d06fb6eb9f5eeff376a2249c4f0fd088d37
legnaleurc <legnaleurc@gmail.com>
2009-07-18 21:11:36 +0800

    [Source] Change build variable.

--- building/ffmpeg/build.sh
+++ building/ffmpeg/build.sh
@@ -1,13 +1,18 @@
 #! /bin/sh
 
+LAME='--enable-libmp3lame'
+#SHARED='--disable-static --enable-shared --disable-stripping'
+OPT='--disable-optimizations'
+
 ../configure \
+--cc='gcc-3.4' \
 --prefix='/usr/local' \
---disable-static \
---enable-shared \
 --enable-gpl \
+$SHARED \
+$LAME \
+$OPT \
 --disable-ffplay \
 --disable-ffserver \
 --disable-network \
 --disable-ipv6 \
---disable-stripping \
 ;
