d81cbe5645bcbc9edd9988f7aeb9406328f7e02c
legnaleurc <legnaleurc@gmail.com>
2009-07-19 17:20:43 +0800

    [Source] Add panel.

--- java/PicKing/src/org/FoolproofProject/Main.java
+++ java/PicKing/src/org/FoolproofProject/Main.java
@@ -3,11 +3,63 @@ package org.FoolproofProject;
 import java.awt.Container;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-public class Main {
+public class Main extends JFrame {
+	
+	private static final long serialVersionUID = 6869079478547863579L;
+	
+	public Main( String title ) {
+		super( title );
+		
+		setDefaultCloseOperation( EXIT_ON_CLOSE );
+		
+		Container pane = getContentPane();
+		pane.setLayout( new BoxLayout( pane, BoxLayout.Y_AXIS ) );
+		
+		initViewPort();
+		initControlPanel();
+	}
+
+	private void initViewPort() {
+		JPanel central = new JPanel();
+		central.setLayout( new BoxLayout( central, BoxLayout.X_AXIS ) );
+		
+		DirectoryTree tree = new DirectoryTree();
+		central.add( tree );
+		
+		FileList list = new FileList( tree );
+		central.add( list );
+		
+		Container pane = getContentPane();
+		pane.add( central );
+	}
+	
+	private void initControlPanel() {
+		JPanel panel = new JPanel();
+		panel.setLayout( new BoxLayout( panel, BoxLayout.X_AXIS ) );
+		
+		NeturalField size = new NeturalField( 4 );
+		panel.add( size );
+		
+		JComboBox unit = new JComboBox();
+		panel.add( unit );
+		unit.addItem( "B" );
+		unit.addItem( "KB" );
+		unit.addItem( "MB" );
+		unit.addItem( "GB" );
+		
+		JButton start = new JButton( "Start" );
+		panel.add( start );
+		
+		Container pane = getContentPane();
+		pane.add( panel );
+	}
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater( new Runnable() {
@@ -18,19 +70,9 @@ public class Main {
 					System.err.println( e.getMessage() );
 				}
 				
-				JFrame window = new JFrame();
-				window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-				window.setSize( 640, 480 );
-				window.setVisible(true);
-				
-				Container pane = window.getContentPane();
-				pane.setLayout( new BoxLayout( pane, BoxLayout.X_AXIS ) );
-				
-				DirectoryTree tree = new DirectoryTree();
-				pane.add( tree );
-				
-				FileList list = new FileList( tree );
-				pane.add( list );
+				Main self = new Main( "PicKing" );
+				self.setSize( 640, 480 );
+				self.setVisible(true);
 			}
 		} );
 	}
---
+++ java/PicKing/src/org/FoolproofProject/NeturalField.java
@@ -0,0 +1,43 @@
+package org.FoolproofProject;
+
+import javax.swing.JTextField;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.PlainDocument;
+
+public class NeturalField extends JTextField {
+
+	private static final long serialVersionUID = -4923755274608244338L;
+	
+	public NeturalField( int columns ) {
+		super( columns );
+	}
+	
+	public Integer getNumber() {
+		return Integer.parseInt( super.getText() );
+	}
+	
+	protected Document createDefaultModel() {
+		return new NeturalDocument();
+	}
+	
+	private class NeturalDocument extends PlainDocument {
+		
+		private static final long serialVersionUID = -3237520404172699543L;
+
+		public void insertString( int offs, String str, AttributeSet a ) throws BadLocationException {
+			if( str == null ) {
+				return;
+			}
+			for( char c : str.toCharArray() ) {
+				if( !Character.isDigit( c ) ) {
+					return;
+				}
+			}
+			super.insertString(offs, str, a);
+		}
+		
+	}
+
+}
