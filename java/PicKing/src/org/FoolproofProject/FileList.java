afc6a4861cdcc78b7d97b73983e4f7827d54557a
legnaleurc <legnaleurc@gmail.com>
2009-07-20 19:26:57 +0800

    [GUI] Add complexty tips.

--- java/PicKing/src/org/FoolproofProject/FileList.java
+++ java/PicKing/src/org/FoolproofProject/FileList.java
@@ -4,14 +4,18 @@ import java.io.File;
 import java.util.Vector;
 
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class FileList extends JPanel {
 
 	private static final long serialVersionUID = -5296371739711677521L;
 	private JList view;
+	private JLabel complexty;
 	
 	public FileList( DirectoryTree tree ) {
 		tree.addFileListListener( this );
@@ -19,6 +23,16 @@ public class FileList extends JPanel {
 		JScrollPane scroll = new JScrollPane( view );
 		setLayout( new BoxLayout( this, BoxLayout.Y_AXIS ) );
 		add( scroll );
+		
+		complexty = new JLabel( "1" );
+		add( complexty );
+		view.getSelectionModel().addListSelectionListener( new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				int items = view.getSelectedIndices().length;
+				complexty.setText( String.valueOf( ( long )Math.pow( 2, items ) ) );
+			}
+		} );
 	}
 	
 	public void setItems( ShortFile path ) {
