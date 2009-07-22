85240be659226400f3ea8058690054beb609740a
legnaleurc <legnaleurc@gmail.com>
2009-07-22 21:02:14 +0800

    [GUI] Add item tip.

--- java/PicKing/src/org/FoolproofProject/FileList.java
+++ java/PicKing/src/org/FoolproofProject/FileList.java
@@ -6,15 +6,19 @@ import java.io.File;
 import java.util.Vector;
 
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class FileList extends JPanel {
 
 	private static final long serialVersionUID = -5296371739711677521L;
 	private JList view;
 	private DirectoryTree parentTree;
+	private JLabel items;
 	
 	public FileList( DirectoryTree tree ) {
 		parentTree = tree;
@@ -32,6 +36,15 @@ public class FileList extends JPanel {
 				}
 			}
 		} );
+		
+		items = new JLabel( "0" );
+		view.getSelectionModel().addListSelectionListener( new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				items.setText( String.valueOf( view.getSelectedIndices().length ) );
+			}
+		} );
+		add( items );
 	}
 	
 	public void setItems( File path ) {
