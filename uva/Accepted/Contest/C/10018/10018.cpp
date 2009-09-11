24234b73e29d7cec17ae67c348ea6b54cbb505a8
legnaleurc <legnaleurc@gmail.com>
2009-09-12 02:20:32 +0800

    [Source] Little change.

--- uva/Accepted/Contest/C/10018/10018.cpp
+++ uva/Accepted/Contest/C/10018/10018.cpp
@@ -5,53 +5,43 @@ using namespace std;
 
 typedef unsigned int uint;
 
-bool radd( string& s, uint& counter );
+uint revadd( string & s ) {
+	uint counter = 0;
+	string clone( s );
 
-int main()
-{
-	uint cases, counter;
-	string temp;
-	
+	reverse( clone.begin(), clone.end() );
+
+	while( clone != s ) {
+		++counter;
+		uint carry = 0;
+		for( int i = s.length() - 1; i >= 0; --i ) {
+			s[i] += clone[i] - '0' + carry;
+			if( s[i] > '9' ) {
+				s[i] -= 10;
+				carry = 1;
+			} else {
+				carry = 0;
+			}
+		}
+		if( carry > 0 ) {
+			s = "1" + s;
+		}
+		clone = s;
+		reverse( clone.begin(), clone.end() );
+	}
+	return counter;
+}
+
+int main() {
+	uint cases;
+	string tmp;
+
 	cin >> cases;
-	while( cases-- )
-	{
-		cin >> temp;
-		counter = 0;
+	while( cases-- ) {
+		cin >> tmp;
 
-		while( radd( temp, counter ) ){}
-
-		cout << counter << " " << temp << endl;
+		cout << revadd( tmp ) << " " << tmp << endl;
 	}
 
 	return 0;
-}
-
-bool radd( string& s, uint& counter )
-{
-	string clone( s.length(), 0 );
-	uint carry = 0;
-
-	reverse_copy( s.begin(), s.end(), clone.begin() );
-
-	if( clone == s && counter > 0 )
-		return false;
-	else
-	{
-		counter++;
-		for( int i = s.length() - 1; i >= 0; i-- )
-		{
-			s[i] += clone[i] - '0' + carry;
-			if( s[i] > '9' )
-			{
-				s[i] -= 10;
-				carry = 1;
-			}
-			else
-				carry = 0;
-		}
-		if( carry == 1 )
-			s = "1" + s;
-
-		return true;
-	}
 }
