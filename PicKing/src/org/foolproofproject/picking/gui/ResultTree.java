2ed70217578150d91f16ca5a2791e08855422f41
legnaleurc <legnaleurc@gmail.com>
2009-10-26 20:00:22 +0800

    [EXPERIMENTAL] Add K3B project export feature.

--- PicKing/src/org/foolproofproject/picking/gui/ResultTree.java
+++ PicKing/src/org/foolproofproject/picking/gui/ResultTree.java
@@ -1,14 +1,24 @@
 package org.foolproofproject.picking.gui;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JFileChooser;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.ToolTipManager;
@@ -17,13 +27,19 @@ import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
 
 public class ResultTree extends JPanel {
 
 	private static final long serialVersionUID = 3366458847085663811L;
+	private static XMLOutputFactory xmlFactory = XMLOutputFactory.newInstance(); 
 	private JTree view;
 	private Hashtable<String,Long> table;
+	private JPopupMenu popup;
+	private DefaultMutableTreeNode selectedNode;
 	
 	public ResultTree() {
 		view = new JTree();
@@ -33,6 +49,33 @@ public class ResultTree extends JPanel {
 		setBorder( BorderFactory.createTitledBorder( "Result" ) );
 		add( scroll );
 		
+		selectedNode = null;
+		
+		// setup popup context menu
+		popup = new JPopupMenu();
+		JMenuItem k3b = new JMenuItem( "Export to K3B project file" );
+		k3b.addActionListener( new ActionListener() {
+			public void actionPerformed( ActionEvent e ) {
+				if( selectedNode == null ) {
+					return;
+				}
+				JFileChooser fc = new JFileChooser();
+				switch( fc.showSaveDialog( view ) ) {
+				case JFileChooser.APPROVE_OPTION:
+					exportToK3B( fc.getSelectedFile(), selectedNode );
+					break;
+				case JFileChooser.CANCEL_OPTION:
+					break;
+				case JFileChooser.ERROR_OPTION:
+					break;
+				default:
+					;
+				}
+			}
+		} );
+		popup.add( k3b );
+		
+		// setup tool tip
 		ToolTipManager.sharedInstance().registerComponent( view );
 		view.setCellRenderer( new DefaultTreeCellRenderer() {
 			private static final long serialVersionUID = 8169622028702532699L;
@@ -52,6 +95,20 @@ public class ResultTree extends JPanel {
 					}
 				}
 				return this;
+			}
+		} );
+		
+		// setup mouse listener
+		view.addMouseListener( new MouseAdapter() {
+			public void mouseClicked( MouseEvent e ) {
+				if( e.getButton() != MouseEvent.BUTTON3 ) {
+					return;
+				}
+				TreePath tp = view.getPathForLocation( e.getX(), e.getY() );
+				if( tp != null && view.getRowForPath( tp ) == 0 ) {
+					selectedNode = ( DefaultMutableTreeNode )tp.getLastPathComponent();
+					popup.show( view, e.getX(), e.getY());
+				}
 			}
 		} );
 		
@@ -132,6 +189,141 @@ public class ResultTree extends JPanel {
 		fout.println( sb.toString() );
 		for( Enumeration< ? > e = node.children(); e.hasMoreElements(); ) {
 			savePath( path.pathByAddingChild( e.nextElement() ), indent + 1, fout );
+		}
+	}
+	
+	private void exportToK3B( File file, DefaultMutableTreeNode node ) {
+		try {
+			XMLStreamWriter xout = xmlFactory.createXMLStreamWriter( new PrintWriter( file, "UTF-8" ) );
+			xout.writeStartDocument( "UTF-8", "1.0" );
+			xout.writeDTD( "k3b_dvd_project" );
+			xout.writeStartElement( "k3b_dvd_project" );
+			
+			xout.writeStartElement( "general" );
+			xout.writeStartElement( "writing_mode" );
+			xout.writeCharacters( "auto" );
+			xout.writeEndElement();
+			xout.writeEmptyElement( "dummy" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "on_the_fly" );
+			xout.writeAttribute( "activated", "yes");
+			xout.writeEmptyElement( "only_create_images" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "remove_images" );
+			xout.writeAttribute( "activated", "yes");
+			xout.writeEndElement();
+
+			xout.writeStartElement( "options" );
+			xout.writeEmptyElement( "rock_ridge" );
+			xout.writeAttribute( "activated", "yes");
+			xout.writeEmptyElement( "joliet" );
+			xout.writeAttribute( "activated", "yes");
+			xout.writeEmptyElement( "udf" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "joliet_allow_103_characters" );
+			xout.writeAttribute( "activated", "yes");
+			xout.writeEmptyElement( "iso_allow_lowercase" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "iso_allow_period_at_begin" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "iso_allow_31_char" );
+			xout.writeAttribute( "activated", "yes");
+			xout.writeEmptyElement( "iso_omit_version_numbers" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "iso_omit_trailing_period" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "iso_max_filename_length" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "iso_relaxed_filenames" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "iso_no_iso_translate" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "iso_allow_multidot" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "iso_untranslated_filenames" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "follow_symbolic_links" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "create_trans_tbl" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "hide_trans_tbl" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeStartElement( "iso_level" );
+			xout.writeCharacters( "2" );
+			xout.writeEndElement();
+			xout.writeEmptyElement( "discard_symlinks" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "discard_broken_symlinks" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "preserve_file_permissions" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "force_input_charset" );
+			xout.writeAttribute( "activated", "no");
+			xout.writeEmptyElement( "do_not_cache_inodes" );
+			xout.writeAttribute( "activated", "yes");
+			xout.writeStartElement( "input_charset" );
+			xout.writeCharacters( "iso8859-1" );
+			xout.writeEndElement();
+			xout.writeStartElement( "whitespace_treatment" );
+			xout.writeCharacters( "noChange" );
+			xout.writeEndElement();
+			xout.writeStartElement( "whitespace_replace_string" );
+			xout.writeCharacters( "_" );
+			xout.writeEndElement();
+			xout.writeStartElement( "data_track_mode" );
+			xout.writeCharacters( "auto" );
+			xout.writeEndElement();
+			xout.writeStartElement( "multisession" );
+			xout.writeCharacters( "auto" );
+			xout.writeEndElement();
+			xout.writeEmptyElement( "verify_data" );
+			xout.writeAttribute( "activated", "yes");
+			xout.writeEndElement();
+
+			xout.writeStartElement( "header" );
+			xout.writeStartElement( "volume_id" );
+			xout.writeCharacters( "PicKing" );
+			xout.writeEndElement();
+			xout.writeStartElement( "volume_set_id" );
+			xout.writeEndElement();
+			xout.writeStartElement( "volume_set_size" );
+			xout.writeCharacters( "1" );
+			xout.writeEndElement();
+			xout.writeStartElement( "volume_set_number" );
+			xout.writeCharacters( "1" );
+			xout.writeEndElement();
+			xout.writeStartElement( "system_id" );
+			xout.writeCharacters( "LINUX" );
+			xout.writeEndElement();
+			xout.writeStartElement( "application_id" );
+			xout.writeCharacters( "K3B THE CD KREATOR (C) 1998-2006 SEBASTIAN TRUEG AND THE K3B TEAM" );
+			xout.writeEndElement();
+			xout.writeStartElement( "publisher" );
+			xout.writeEndElement();
+			xout.writeStartElement( "preparer" );
+			xout.writeEndElement();
+			xout.writeEndElement();
+			
+			xout.writeStartElement( "files" );
+			for( Enumeration< ? > e = node.children(); e.hasMoreElements(); ) {
+				DefaultMutableTreeNode child = (DefaultMutableTreeNode)e.nextElement();
+				ShortFile sf = (ShortFile)child.getUserObject();
+				xout.writeStartElement( "file" );
+				xout.writeCharacters( sf.getAbsolutePath() );
+				xout.writeEndElement();
+			}
+			xout.writeEndElement();
+			
+			xout.writeEndElement();
+			xout.writeEndDocument();
+			xout.flush();
+			xout.close();
+		} catch (XMLStreamException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
