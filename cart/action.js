a699c6c3bd1792b0510caa9c6cbb91436729f7e1
legnaleurc <legnaleurc@gmail.com>
2010-09-18 12:30:49 +0800

    [Project] Add cart.

---
+++ cart/action.js
@@ -0,0 +1,63 @@
+$( function() {
+
+	function newRow( data ) {
+		return $( '<tr/>' ).append( $( '<td/>' ).append( $( '<input/>' ).attr( {
+				type: 'checkbox'
+			} ).addClass( 'check' ) ) ).append( $( '<td/>' ).addClass( 'title' ).append( $( '<a/>' ).attr( {
+			href: data.uri,
+			rel: 'external'
+		} ).text( data.title ).click( function( event ) {
+			event.preventDefault();
+			window.open( $( this ).attr( 'href' ), '_blank' );
+		} ) ) ).append( $( '<td/>' ).addClass( 'date' ).text( data.date ) ).append( $( '<td/>' ).append( $( '<input/>' ).attr( {
+			type: 'button',
+			value: 'Delete'
+		} ).click( function( event ) {
+			var self = $( this );
+			var key = self.parent().siblings( '.title' ).text();
+			jQuery.post( 'delete.php', {
+				title: key
+			}, function( data, textStatus ) {
+				if( textStatus == 'success' ) {
+					self.parent().parent().remove();
+				} else {
+					window.alert( data );
+				}
+			}, 'json' );
+		} ) ) );
+	}
+
+	jQuery.getJSON( 'load.php', function( data, textStatus ) {
+		$( data ).each( function( index, row ) {
+			$( '#cart' ).append( newRow( row ) );
+		} );
+	} );
+
+	$( '#submit' ).click( function() {
+		var args = {
+			title: $( '#title' ).val(),
+			uri: $( '#uri' ).val(),
+			date: $( '#date' ).val()
+		};
+		jQuery.post( 'save.php', args, function( data ) {
+			var done = false;
+			$( '#cart' ).children().each( function( index, self ) {
+				self = $( self );
+				if( self.children().filter( '.date' ).text() > args.date ) {
+					self.before( newRow( args ) );
+					done = true;
+					return false;
+				}
+			} );
+			if( !done ) {
+				$( '#cart' ).append( newRow( args ) );
+			}
+			$( '#stdin input[type=text]' ).val( '' );
+		}, 'json' );
+	} );
+
+	$( '#stdin input[type=text]' ).focus( function() {
+		this.select();
+	} );
+
+} );
