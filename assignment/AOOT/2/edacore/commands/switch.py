acfe567b3417ccde06d6a31aceb0dbcc1be12e4c
Wei Cheng Pan <legnaleurc@gmail.com>
2012-05-07 15:19:30 +0800

    Add switch command.

--- assignment/AOOT/2/edacore/commands/switch.py
+++ assignment/AOOT/2/edacore/commands/switch.py
@@ -1,10 +1,13 @@
 import edacore, re
+from edacore import logic
 
-def switchGate():
-	pass
+def switchGate( gateName, logicType ):
+	gate = edacore.currentCircuit.getGate( gateName )
+	gate.logic = logic.createLogic( logicType )
+	print 'The type of the gate "{0}" has become {1}'.format( gateName, logicType )
 
 def parse( line ):
 	m = re.match( r'^switch\s+gate\s+(\w+)\s+(AND|NOT|OR|NAND|NOR)$', line )
 	if m == None:
 		return None
-	return switchGate
+	return lambda: switchGate( m.group( 1 ), m.group( 2 ) )
