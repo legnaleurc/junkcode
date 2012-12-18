acc00adf122ce6320aa539c8f4b7559abfbadc5b
Wei Cheng Pan <legnaleurc@gmail.com>
2012-12-19 01:54:30 +0800

    Add more operator.

--- talk/demo/04_big_integer/include/biginteger.hpp
+++ talk/demo/04_big_integer/include/biginteger.hpp
@@ -8,6 +8,18 @@ namespace bi {
 
 class BigInteger {
 public:
+	class Division {
+	public:
+		const BigInteger & getQuotient() const;
+		const BigInteger & getRemainder() const;
+
+	private:
+		friend class BigInteger;
+		Division( const BigInteger & q, const BigInteger & r );
+		class Private;
+		std::shared_ptr< Private > p_;
+	};
+
 	BigInteger();
 	BigInteger( const BigInteger & that );
 	BigInteger( BigInteger && that );
@@ -23,11 +35,16 @@ public:
 
 	BigInteger & operator +=( const BigInteger & that );
 	BigInteger & operator -=( const BigInteger & that );
+	BigInteger & operator *=( const BigInteger & that );
+	BigInteger & operator /=( const BigInteger & that );
+
+	Division divide( const BigInteger & that ) const;
 
 	void swap( BigInteger & that );
 
 	template< typename T >
 	T to() const;
+
 private:
 	friend BigInteger operator -( BigInteger && a );
 	friend bool operator <( const BigInteger & a, const BigInteger & b );
@@ -52,6 +69,8 @@ bool operator !=( const BigInteger & a, const BigInteger & b );
 
 BigInteger operator +( const BigInteger & a, const BigInteger & b );
 BigInteger operator -( const BigInteger & a, const BigInteger & b );
+BigInteger operator *( const BigInteger & a, const BigInteger & b );
+BigInteger operator /( const BigInteger & a, const BigInteger & b );
 BigInteger & operator ++( BigInteger & a );
 BigInteger operator ++( BigInteger & a, int );
 BigInteger & operator --( BigInteger & a );
