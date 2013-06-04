9ac8324f2bbeff0ef06dd87de7af303356d07601
Wei Cheng Pan <legnaleurc@gmail.com>
2013-06-04 15:23:22 +0800

    Update suggested pages.

--- sandbox/javascript/gm_scripts/Strip_Facebook_Posts.user.js
+++ sandbox/javascript/gm_scripts/Strip_Facebook_Posts.user.js
@@ -2,6 +2,7 @@
 // @name        Strip Facebook Posts
 // @namespace   sandbox
 // @run-at      document-start
+// @grant       none
 // @match       https://www.facebook.com/*
 // @exclude     https://www.facebook.com/ajax/*
 // @exclude     https://www.facebook.com/ai.php
@@ -26,10 +27,10 @@
         var observer = new MutationObserver(function (mutations) {
             mutations.forEach(function (mutation) {
                 Array.prototype.forEach.call(mutation.addedNodes, function (child) {
-                    var i = child.querySelector('span.fwn.fcg:not(.timestamp)');
-                    if (i && /suggested post/i.test(i.textContent)) {
-                        i = child.querySelector('div.actorName a');
-                        console.info('removed suggested post: ' + i.textContent);
+                    var i = child.querySelector('span.fwn.fcg:not(.timestamp), span._6_r');
+                    if (i && /suggested p(ost|age)/i.test(i.textContent)) {
+                        i = child.querySelector('div.actorName a, span._706 a');
+                        console.info('removed suggested post/page: ' + i.textContent);
                         target.removeChild(child);
                     }
                 });
