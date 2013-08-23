8123bcd1124700cbf723ead1fa504d1a08157535
Wei-Cheng Pan <legnaleurc@gmail.com>
2013-08-23 17:37:31 +0800

    Tiny improvment.

--- sandbox/c++/sfx/sfx.cpp
+++ sandbox/c++/sfx/sfx.cpp
@@ -1,47 +1,50 @@
+#include <cstdlib>
+
+#include <algorithm>
 #include <fstream>
-#include <string>
-#include <vector>
 #include <iostream>
 #include <iterator>
-#include <algorithm>
-#include <cstdlib>
+#include <string>
+#include <vector>
+
 
 const char * OUT_FILE = "/tmp/injection";
 
-int main( int argc, char * argv[] ) {
+
+int main (int argc, char * argv[]) {
 	using namespace std;
 	typedef char Byte;
-	typedef vector< Byte > ByteArray;
-	ifstream fin( argv[0], ios::binary );
+	typedef vector<Byte> ByteArray;
 
-	if( fin.is_open() ) {
-		ByteArray binary;
-		copy( istreambuf_iterator< Byte >( fin ), istreambuf_iterator< Byte >(), back_inserter( binary ) );
-		fin.close();
+	ifstream fin(argv[0], ios::binary);
 
-		string magic( "!_METAMAGIC__" );
-		magic[0] = '_';
-		ByteArray::iterator result = search( binary.begin(), binary.end(), magic.begin(), magic.end() );
-		if( result == binary.end() ) {
-			cerr << "Can not find piece!" << endl;
-			return 1;
-		} else {
-			advance( result, magic.length() );
-			ofstream fout( OUT_FILE, ios::binary );
-			if( fout.is_open() ) {
-				copy( result, binary.end(), ostreambuf_iterator< Byte >( fout ) );
-				fout.close();
-				ByteArray().swap( binary );
-				system( ( string( "chmod a+x " ) + OUT_FILE + " && " + OUT_FILE ).c_str() );
-			} else {
-				cerr << "Can not write `" << OUT_FILE << "\'!" << endl;
-				return 1;
-			}
-		}
-	} else {
+	if (!fin.is_open()) {
 		cerr << "Can not open " << argv[0] << "!" << endl;
 		return 1;
 	}
+
+	ByteArray binary;
+	copy(istreambuf_iterator<Byte>(fin), istreambuf_iterator<Byte>(), back_inserter(binary));
+	fin.close();
+
+	string magic = "!_METAMAGIC__";
+	magic[0] = '_';
+
+	auto result = search(binary.begin(), binary.end(), magic.begin(), magic.end());
+	if (result == binary.end()) {
+		cerr << "Can not find piece!" << endl;
+		return 1;
+	}
+
+	advance(result, magic.length());
+	ofstream fout(OUT_FILE, ios::binary);
+	if (!fout.is_open()) {
+		cerr << "Can not write `" << OUT_FILE << "\'!" << endl;
+		return 1;
+	}
+
+	copy(result, binary.end(), ostreambuf_iterator<Byte>(fout));
+	fout.close();
 
 	return 0;
 }
