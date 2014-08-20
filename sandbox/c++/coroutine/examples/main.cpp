7eb5718cca0ad235460038acaa232f115fe1044c
Wei-Cheng Pan <legnaleurc@gmail.com>
2014-08-20 13:45:09 +0800

    Add comment.

--- sandbox/c++/coroutine/examples/main.cpp
+++ sandbox/c++/coroutine/examples/main.cpp
@@ -14,16 +14,19 @@ int main(int argc, char *argv[]) {
 
     auto b = new QtCoroutine([](const QtCoroutine::Yield & yield)->void {
         qDebug() << "wait for http server open ...";
+        // NOTE yield to main event loop until 1000ms passed
         yield(1000);
 
         qDebug() << "make request";
         QNetworkAccessManager nasm;
         QNetworkRequest r(QUrl("http://localhost:1080/"));
         auto reply = nasm.get(r);
+        // NOTE yield to main event loop until request finished
         yield(reply, SIGNAL(finished()));
         auto c = reply->readAll();
         qDebug() << c;
     });
+    // NOTE when coroutine finished (i.e. reaches end or return), `finished()` emitted
     b->connect(b, SIGNAL(finished()), SLOT(deleteLater()));
     a.connect(b, SIGNAL(finished()), SLOT(quit()));
     b->start();
