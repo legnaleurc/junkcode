667e3afe01964d843eef6a1219b7f40c970e60f3
Wei-Cheng Pan <legnaleurc@gmail.com>
2015-09-24 06:46:48 +0900

    update

--- sandbox/c++/fbv/src/main.cpp
+++ sandbox/c++/fbv/src/main.cpp
@@ -4,19 +4,28 @@
 #include <QtWidgets/QGraphicsScene>
 #include <QtWidgets/QGraphicsView>
 #include <QtWidgets/QDesktopWidget>
+#include <QtWidgets/QStackedWidget>
+#include <QtWidgets/QFileDialog>
 
 
 int main (int argc, char * argv[]) {
     QApplication application(argc, argv);
+    auto rect = application.desktop()->availableGeometry();
 
     fbv::KeyboardHandler kh;
 
-    QGraphicsScene scene;
+    // root
+    QStackedWidget container;
+    QGraphicsScene * scene = new QGraphicsScene(&kh);
+    QGraphicsView * view = new QGraphicsView(scene, &container);
+//    QFileDialog * fileDialog = new QFileDialog(&container, Qt::Widget);
 
-    QGraphicsView view(&scene);
-    auto rect = application.desktop()->availableGeometry();
-    view.setGeometry(rect);
-    view.show();
+    container.addWidget(view);
+//    container.addWidget(fileDialog);
+    container.setCurrentIndex(0);
+
+    container.setGeometry(rect);
+    container.show();
 
     return application.exec();
 }
