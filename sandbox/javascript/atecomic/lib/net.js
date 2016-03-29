0b50c14bfcd50dbcdbd2afb431b03fdc160ccdcb
Wei-Cheng Pan <wpan@mozilla.com>
2016-03-29 15:21:44 +0800

    simplify http client

--- sandbox/javascript/atecomic/lib/net.js
+++ sandbox/javascript/atecomic/lib/net.js
@@ -1,31 +1,15 @@
 'use strict';
 
 
-var request = require('request');
-var iconv = require('iconv-lite');
+var fetch = require('node-fetch');
 var jsdom = require('jsdom');
 
 
-function ajax (method, url) {
-  if (method === 'GET') {
-    return new Promise((resolve, reject) => {
-      request.get({
-          url: url,
-          encoding: null,
-        }, function (e, r, body) {
-        var newBody = iconv.decode(new Buffer(body), "big5");
-        resolve(newBody);
-      });
-    });
-  } else {
-    return null;
-  }
-}
-
-
 function getPage (url) {
-  return ajax('GET', url).then((body) => {
-    var document = jsdom.jsdom(body);
+  return fetch(url).then((response) => {
+    return response.text();
+  }).then((html) => {
+    var document = jsdom.jsdom(html);
     return document;
   });
 }
