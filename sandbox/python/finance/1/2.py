1643ddb6d0bca539cbe7a55a385b238c256d8b9e
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-05-28 18:04:11 +0900

    fix

--- sandbox/python/finance/1/2.py
+++ sandbox/python/finance/1/2.py
@@ -15,7 +15,7 @@ def calc(age_begin, age_end, gain, gain_rate, paid, paid_rate):
     total_gain = 0
     # 總支出
     total_paid = 0
-    for i in range(0, age_end - age_begin):
+    for i in range(1, age_end - age_begin + 1):
         # 年度薪資
         year_gain = gain * 12 * (gain_rate ** i)
         # 年度支出
@@ -24,7 +24,7 @@ def calc(age_begin, age_end, gain, gain_rate, paid, paid_rate):
         total_gain += year_gain
         total_paid += year_paid
 
-    return total_gain, total_paid, total_gain - total_paid
+    return year_gain / 12, year_paid / 12, total_gain - total_paid
 
 
 def main(args=None):
@@ -32,9 +32,9 @@ def main(args=None):
         args = sys.argv
 
     gain, paid, total = calc(25, 28, 30000, 3, 19000, 2)
-    print('薪質所得 {0} 元'.format(gain))
-    print('支出額 {0} 元'.format(paid))
-    print('存款餘額 {0} 元'.format(total))
+    print('薪質所得 {0} 元'.format(int(round(gain))))
+    print('支出額 {0} 元'.format(int(round(paid))))
+    print('存款餘額 {0} 元'.format(int(round(total))))
 
     return 0
 
