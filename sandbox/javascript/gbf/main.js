f9535010c22837c6d53179734d389b9ff2c0afb5
Wei-Cheng Pan <wpan@mozilla.com>
2016-05-31 17:19:30 +0800

    update

--- sandbox/javascript/gbf/main.js
+++ sandbox/javascript/gbf/main.js
@@ -1,28 +1,50 @@
-(function (context, $) {
-  'use strict';
+// ==UserScript==
+// @name         GBF
+// @namespace    foolproofproject
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://game.granbluefantasy.jp/*
+// @grant        unsafeWindow
+// ==/UserScript==
 
+'use strict';
+
+
+function factory ($) {
   var GBF = {
     ajaxActionList: [
       {
         path: /^\/deckcombination\/deck_combination_list\/(\d+)$/,
-        action: function (args, data) {
+        action: function (args, request, response) {
           var metasetNumber = parseInt(args[1], 10);
-          var sets = data.list;
+          var sets = response.list;
 
           GBF.addMetaset(metasetNumber, sets);
         },
       },
       {
         path: /^\/weapon\/list\/\d+\/0$/,
-        action: function (args, data) {
-          var weapons = data.list;
+        action: function (args, request, response) {
+          var weapons = response.list;
 
           GBF.addWeapons(weapons);
+        },
+      },
+      {
+        path: /^\/weapon\/weapons_parameter$/,
+        action: function (args, request, response) {
+          if (request.parameter_name !== 'skill_level') {
+            return;
+          }
+
+          GBF.updateWeaponSkillLevel(response);
         },
       },
     ],
     metasets: {},
     weapons: {},
+    paramToWeapon: {},
   };
 
 
@@ -65,28 +87,42 @@
     for (var weapon of weapons) {
       var id = weapon.master.id;
       GBF.weapons[id] = weapon;
+      GBF.paramToWeapon[weapon.param.id] = id;
     }
   };
 
 
-  GBF.load = function () {
-    $(document).on('ajaxSuccess', function (event, xhr, jqXHR, data) {
-      var url = parseURL(jqXHR.url);
-      var action = dispatch(url.path);
-      action(data);
+  GBF.updateWeaponSkillLevel = function (weaponSkillMap) {
+    for (var id in weaponSkillMap) {
+      if (!GBF.paramToWeapon[id]) {
+        continue;
+      }
+      var weaponID = GBF.paramToWeapon[id];
+      GBF.weapons[weaponID].skillLevel = weaponSkillMap[id];
+    }
+  };
+
+
+  GBF.run = function () {
+    $(document).on('ajaxSuccess', function (event, xhr, jqXHR, response) {
+      try {
+        var url = parseURL(jqXHR.url);
+        var action = dispatch(url.path);
+        var request = jqXHR.data ? JSON.parse(jqXHR.data) : null;
+        action(request, response);
+      } catch (e) {
+        console.error(e);
+      }
     });
   };
 
+  return GBF;
 
-  GBF.unload = function () {
-    $(document).off('ajaxSuccess');
-  };
+}
 
 
-  if (context.__gbf__) {
-    context.__gbf__.unload();
-  }
-  context.__gbf__ = GBF;
-  GBF.load();
-
-})(window, jQuery);
+window.addEventListener('load', function () {
+  var GBF = factory(unsafeWindow.$);
+  GBF.run();
+  unsafeWindow.__GBF__ = GBF;
+});
