7435d40569ece419fd3ea30599a803568c429f04
Wei-Cheng Pan <wpan@mozilla.com>
2016-09-01 17:29:08 +0800

    update

--- sandbox/python/gdbinit_firefox.py
+++ sandbox/python/gdbinit_firefox.py
@@ -1,6 +1,9 @@
 # ex: et ts=4 sts=4 sw=4 :
 
+import re
+
 import gdb
+import gdb.xmethod
 
 
 def nested_get(value, property_list, default=None):
@@ -31,7 +34,7 @@ class DereferenceCommand(gdb.Command):
 
         try:
             print(value.dereference())
-            return
+            return gdb.Value(111)
         except Exception:
             pass
 
@@ -70,4 +73,54 @@ class DereferenceCommand(gdb.Command):
         print('no matching pointer')
 
 
+class NetscapePointerMatcher(gdb.xmethod.XMethodMatcher):
+
+    def __init__(self):
+        super(NetscapePointerMatcher, self).__init__('NetscapePointerMatcher')
+
+        self._methods = (
+            NetscapePointer(),
+        )
+
+    def match(self, class_type, method_name):
+        m = re.match(r'^nsCOMPtr<.*>$', class_type.tag)
+        if not m:
+            return None
+        workers = []
+        for method in self._methods:
+            if method.enabled:
+                worker = method.get_worker(method_name)
+                if worker:
+                    workers.append(worker)
+        return workers
+
+
+class NetscapePointer(gdb.xmethod.XMethod):
+
+    def __init__(self):
+        super(NetscapePointer, self).__init__('nsCOMPtr')
+
+    def get_worker(self, method_name):
+        if method_name == 'operator*':
+            return NetscapePointerDereference()
+
+
+class NetscapePointerDereference(gdb.xmethod.XMethodWorker):
+
+    def __init__(self):
+        super(NetscapePointerDereference, self).__init__()
+
+'''
+    def get_arg_types(self):
+        return None
+
+    def get_result_type(self, obj):
+        return obj.type.template_argument(0)
+'''
+
+    def __call__(self, obj):
+        return obj['mRawPtr'].dereference()
+
+
 DereferenceCommand()
+gdb.xmethod.register_xmethod_matcher(None, NetscapePointerMatcher())
