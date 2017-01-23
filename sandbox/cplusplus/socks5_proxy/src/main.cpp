70f8f7d11bf5da03b8135712945c276757ad7720
Wei-Cheng Pan <wpan@mozilla.com>
2017-01-23 14:36:24 +0800

    add program options

--- sandbox/cplusplus/socks5_proxy/src/main.cpp
+++ sandbox/cplusplus/socks5_proxy/src/main.cpp
@@ -4,8 +4,37 @@
 #include <csignal>
 #include <iostream>
 
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
 
 int main(int argc, char * argv[]) {
+    po::options_description od("SOCKS5 proxy");
+    od.add_options()
+        ("help,h", "show this message")
+        ("listen,l", po::value<uint16_t>(), "listen to the port")
+        ("host,H", po::value<std::string>(), "forward to this host")
+        ("port,p", po::value<uint16_t>(), "forward to this port")
+    ;
+
+    po::variables_map vm;
+    try {
+        auto rv = po::parse_command_line(argc, argv, od);
+        po::store(rv, vm);
+    } catch (std::exception & e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << od << std::endl;
+        return 0;
+    }
+
+    return 0;
+
     s5p::IOLoop loop;
 
     s5p::SignalHandler signals(loop, SIGINT);
