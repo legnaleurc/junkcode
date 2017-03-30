a0834634b448c0775b244033870dd282f5d471c6
Wei-Cheng Pan <wpan@mozilla.com>
2017-03-30 18:02:15 -0400

    update

--- sandbox/cplusplus/logging/src/main.cpp
+++ sandbox/cplusplus/logging/src/main.cpp
@@ -1,9 +1,23 @@
 #include <boost/log/core.hpp>
+#include <boost/log/sinks.hpp>
+#include <boost/make_shared.hpp>
+
+#include <fstream>
 
 int main (int argc, char * argv[]) {
   namespace bl = boost::log;
 
   auto core = bl::core::get();
+
+  auto backend = boost::make_shared<bl::sinks::text_ostream_backend>();
+  auto fout = boost::make_shared<std::ofstream>("/tmp/logging.txt");
+  backend->add_stream(fout);
+  backend->auto_flush(true);
+
+  typedef bl::sinks::synchronous_sink<bl::sinks::text_ostream_backend> SinkType;
+  auto sink = boost::make_shared<SinkType>(backend);
+
+  core->add_sink(sink);
 
   return 0;
 }
