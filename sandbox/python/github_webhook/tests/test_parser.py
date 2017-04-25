cc17106c8ad9b3c31e58c8be1f6dca629f4f924d
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-25 15:09:33 +0800

    update

--- sandbox/python/github_webhook/tests/test_parser.py
+++ sandbox/python/github_webhook/tests/test_parser.py
@@ -5,6 +5,13 @@ from triage_bot import parser as tbp
 
 class TestParser(ut.TestCase):
 
-    def testExtractURLs(self):
+    def testExtractURLs_SingleDomain(self):
         url = tbp.extractURLs('www.google.com')
         self.assertEqual(url, ['www.google.com'])
+
+    def testExtractURLs_MultipleDomain(self):
+        url = tbp.extractURLs('front www.google.com and www.mozilla.org end~~')
+        self.assertEqual(url, [
+            'www.google.com',
+            'www.mozilla.org',
+        ])
