f428a7d3604dc2dd97b842aee7b70be6c4f516d4
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-05-14 05:12:44 +0900

    add

---
+++ userscript/window_close.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name        window.close
+// @namespace   sandbox
+// @include     http://*
+// @include     https://*
+// @version     1
+// @grant       unsafeWindow
+// @run-at      document-start
+// ==/UserScript==
+
+'use strict';
+
+function getDialog () {
+  var div = document.querySelector('#close-overlay');
+  if (div) {
+    return div;
+  }
+  var div = document.createElement('div');
+  div.id = 'close-overlay';
+  div.style.position = 'fixed';
+  div.style.top = 0;
+  div.style.left = 0;
+  div.style.width = '100%';
+  div.style.zIndex = 65535;
+  div.style.backgroundColor = 'black';
+  div.style.color = 'white';
+  document.body.appendChild(div);
+  return div;
+}
+
+function appendDialog (url) {
+  var div = getDialog();
+  var p = document.createElement('p');
+  p.textContent = url;
+  div.appendChild(p);
+}
+
+unsafeWindow.open = exportFunction(function (url) {
+  console.info('window.open', url);
+  appendDialog(url);
+  var p = new Proxy({}, {
+    get (target, name) {
+      console.info('get', target, name);
+      return undefined;
+    },
+  });
+  return p;
+}, unsafeWindow);
