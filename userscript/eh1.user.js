51a6ea33de9cd2e9bd532fad59e8da84a9367ed5
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-05-22 22:26:56 +0900

    update

--- userscript/eh1.user.js
+++ userscript/eh1.user.js
@@ -12,24 +12,33 @@
 main();
 
 
-async function main () {
+function main () {
   const items = document.querySelectorAll('.id1');
 
   items.forEach((item) => {
-    let marks = item.querySelectorAll('.tft');
-    marks = Array.prototype.filter.call(marks, (mark) => {
-      return mark.title === 'female:netorare';
-    });
-    if (marks.length <= 0) {
-      marks = item.querySelectorAll('.id41');
-      marks = Array.prototype.filter.call(marks, (mark) => {
-        return mark.title === 'Western';
-      });
-      if (marks.length <= 0) {
-        return;
-      }
+    if (hasFlags(item) || hasCategories(item)) {
+      item.style.opacity = 0.2;
     }
-
-    item.style.opacity = 0.2;
   });
+}
+
+
+function hasFlags (item) {
+  let marks = item.querySelectorAll('.tft');
+  marks = Array.prototype.filter.call(marks, (mark) => {
+    return [
+      'female:netorare',
+      'parody:touhou project',
+    ].some(_ => _ === mark.title);
+  });
+  return marks.length > 0;
+}
+
+
+function hasCategories (item) {
+  let marks = item.querySelectorAll('.id41');
+  marks = Array.prototype.filter.call(marks, (mark) => {
+    return mark.title === 'Western';
+  });
+  return marks.length > 0;
 }
