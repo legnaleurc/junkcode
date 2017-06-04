bbac9da654600cd16097bf5757943ac295a2893d
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-06-04 11:08:49 +0900

    update

--- sandbox/python/wcpan.gd/wcpan/gd/network.py
+++ sandbox/python/wcpan.gd/wcpan/gd/network.py
@@ -1,4 +1,5 @@
 import json
+import time
 import urllib.parse as up
 
 
@@ -8,6 +9,17 @@ class Network(object):
         self._http = http
 
     def get(self, path, args=None):
+        while True:
+            rv = self._get(path, args)
+            if rv.status[0] == '2':
+                return rv
+            if rv.status == '403':
+                # FIXME blocking thread, backoff strategy
+                time.sleep(5)
+                continue
+            raise Exception('request error')
+
+    def _get(self, path, args):
         if args is not None:
             args = up.urlencode(args)
             path = '{0}?{1}'.format(path, args)
@@ -20,6 +32,10 @@ class Response(object):
     def __init__(self, response, content):
         self._response = response
         self._content = content
+
+    @property
+    def status(self):
+        return self._response['status']
 
     @property
     def json_(self):
