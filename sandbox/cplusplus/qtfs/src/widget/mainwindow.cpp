0fd3ea06d9efcca9ce7e13aee6c006fadac8b756
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-07-05 11:02:57 +0800

    update

--- sandbox/cplusplus/qtfs/src/widget/mainwindow.cpp
+++ sandbox/cplusplus/qtfs/src/widget/mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
 
+#include <QtWidgets/QFileSystemModel>
+#include <QtCore/QDir>
+
 
 namespace qtfs {
 
@@ -8,8 +11,12 @@ class MainWindow::Private : public QObject {
 public:
     Private(MainWindow * parent);
 
+    void setupModel(QFileSystemModel * model, QTreeView * view);
+
     MainWindow * _;
     Ui::MainWindow ui;
+    QFileSystemModel * leftModel;
+    QFileSystemModel * rightModel;
 };
 
 }
@@ -29,6 +36,17 @@ MainWindow::Private::Private(MainWindow * parent)
     : QObject(parent)
     , _(parent)
     , ui()
+    , leftModel(new QFileSystemModel(this))
+    , rightModel(new QFileSystemModel(this))
 {
     this->ui.setupUi(this->_);
+
+    this->setupModel(this->leftModel, this->ui.leftView);
+    this->setupModel(this->rightModel, this->ui.rightView);
+}
+
+
+void MainWindow::Private::setupModel(QFileSystemModel * model, QTreeView * view) {
+    model->setRootPath(QDir::homePath());
+    view->setModel(model);
 }
