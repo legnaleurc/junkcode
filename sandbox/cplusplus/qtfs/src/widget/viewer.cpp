dd44b3fbe1a2deb2adfed42bafc5d949553134f8
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-07-05 12:33:45 +0800

    update

--- sandbox/cplusplus/qtfs/src/widget/viewer.cpp
+++ sandbox/cplusplus/qtfs/src/widget/viewer.cpp
@@ -9,9 +9,16 @@
 namespace qtfs {
 
 class Viewer::Private : public QObject {
+    Q_OBJECT
+
 public:
     explicit Private(Viewer * parent);
+    void setPath(const QString & path);
 
+public slots:
+    void commitPath();
+
+public:
     Viewer * _;
     Ui::Viewer ui;
     QFileSystemModel * model;
@@ -40,8 +47,30 @@ Viewer::Private::Private(Viewer * parent)
 
     this->model->setRootPath(QDir::rootPath());
     this->ui.view->setModel(this->model);
-    this->ui.view->setRootIndex(this->model->index(QDir::homePath()));
 
-    QCompleter * completer = new QCompleter(this->model, this);
+    auto completer = new QCompleter(this->model, this);
     this->ui.path->setCompleter(completer);
+
+    this->connect(this->ui.path, SIGNAL(returnPressed()), SLOT(commitPath()));
+
+    this->setPath(QDir::homePath());
 }
+
+
+void Viewer::Private::setPath(const QString & path) {
+    auto index = this->model->index(path);
+    if (!index.isValid()) {
+        return;
+    }
+    this->ui.path->setText(path);
+    this->ui.view->setRootIndex(index);
+}
+
+
+void Viewer::Private::commitPath() {
+    auto path = this->ui.path->text();
+    this->setPath(path);
+}
+
+
+#include "viewer.moc"
