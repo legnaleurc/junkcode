aa3ccb5b015dfae38398c2f3b5dd0f84465fdafb
Wei-Cheng Pan <wpan@mozilla.com>
2017-07-12 10:28:42 +0800

    update

--- sandbox/python/dfsd/dfsd/view.py
+++ sandbox/python/dfsd/dfsd/view.py
@@ -1,3 +1,5 @@
+import xml.etree.ElementTree as ET
+
 from tornado import web as tw
 
 import wcpan.logger as wl
@@ -5,19 +7,69 @@ import wcpan.logger as wl
 
 class Root(tw.RequestHandler):
 
-    SUPPORTED_METHODS = tw.RequestHandler.SUPPORTED_METHODS + ('MKCOL', 'PROPFIND', 'PROPPATCH', 'ORDERPATCH', 'LOCK', 'UNLOCK', 'COPY', 'MOVE')
-
-    def get(self):
-        pass
+    SUPPORTED_METHODS = tw.RequestHandler.SUPPORTED_METHODS + ('MKCOL', 'PROPFIND', 'PROPPATCH', 'ORDERPATCH', 'COPY', 'MOVE', 'TRACE', 'REPORT', 'LOCK', 'UNLOCK')
 
     def options(self):
         wl.DEBUG('dfsd') << self.request.headers
         wl.DEBUG('dfsd') << self.request.body
 
-        self.set_header('Allow', 'OPTIONS, GET, HEAD, POST, DELETE, PATCH, PUT')
-        self.set_header('Allow', 'MKCOL, PROPFIND, PROPPATCH, ORDERPATCH, LOCK, UNLOCK, COPY, MOVE')
-        self.set_header('DAV', '1, 2, ordered-collections')
+        self.set_header('Allow', 'OPTIONS, GET, HEAD, POST, DELETE, PATCH, PUT, MKCOL, PROPFIND, PROPPATCH, ORDERPATCH, COPY, MOVE, TRACE, REPORT, LOCK, UNLOCK')
+        self.set_header('DAV', '1, 2')
+
+    def get(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def head(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def post(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def delete(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def patch(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def put(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def mkcol(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
 
     def propfind(self):
+        depth = self.request.headers['Depth']
+        data = self.request.body
+        data = ET.fromstring(data)
+        wl.DEBUG('dfsd') << data
+
+    def proppatch(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def orderpatch(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def lock(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def unlock(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def copy(self):
+        wl.DEBUG('dfsd') << self.request.headers
+        wl.DEBUG('dfsd') << self.request.body
+
+    def move(self):
         wl.DEBUG('dfsd') << self.request.headers
         wl.DEBUG('dfsd') << self.request.body
