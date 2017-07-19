e5e9b2ba68bad2e160ac4ad77de1d979e4cc0f80
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-07-19 14:50:07 +0800

    update

--- sandbox/python/gdbinit_firefox.py
+++ sandbox/python/gdbinit_firefox.py
@@ -50,26 +50,6 @@ class DereferenceCommand(gdb.Command):
         except Exception:
             pass
 
-        if value.type.name == 'mozilla::StyleSheetHandle::RefPtr':
-            tmp = nested_get(value, ['mHandle', 'mPtr', 'mValue'])
-            if tmp:
-                tmp = long(tmp)
-                if tmp & 0x1:
-                    tmp = tmp - 1
-                    type_ = 'mozilla::ServoStyleSheet'
-                else:
-                    type_ = 'mozilla::CSSStyleSheet'
-
-                tmp = gdb.Value(tmp)
-                type_ = gdb.lookup_type(type_)
-                tmp = tmp.cast(type_.pointer())
-
-                print(tmp.dereference())
-            else:
-                print('(mozilla::StyleSheet *) 0x0')
-
-            return
-
         print('no matching pointer')
 
 
@@ -83,7 +63,7 @@ class NetscapePointerMatcher(gdb.xmethod.XMethodMatcher):
         )
 
     def match(self, class_type, method_name):
-        m = re.match(r'^nsCOMPtr<.*>$', class_type.tag)
+        m = re.match(r'^(Ref|nsCOM)Ptr<.*>$', class_type.tag)
         if not m:
             return None
         workers = []
@@ -98,7 +78,7 @@ class NetscapePointerMatcher(gdb.xmethod.XMethodMatcher):
 class NetscapePointer(gdb.xmethod.XMethod):
 
     def __init__(self):
-        super(NetscapePointer, self).__init__('nsCOMPtr')
+        super(NetscapePointer, self).__init__('NetscapePointer')
 
     def get_worker(self, method_name):
         if method_name == 'operator*':
@@ -117,91 +97,5 @@ class NetscapePointerDereference(gdb.xmethod.XMethodWorker):
         return obj['mRawPtr'].dereference()
 
 
-class StyleSheetHandleMatcher(gdb.xmethod.XMethodMatcher):
-
-    def __init__(self):
-        super(StyleSheetHandleMatcher, self).__init__('StyleSheetHandleMatcher')
-
-        self._methods = (
-            StyleSheetHandle(),
-        )
-
-    def match(self, class_type, method_name):
-        if class_type.tag != 'mozilla::HandleRefPtr<mozilla::StyleSheetHandle>':
-            return None
-        workers = []
-        for method in self._methods:
-            if method.enabled:
-                worker = method.get_worker(method_name)
-                if worker:
-                    workers.append(worker)
-        return workers
-
-
-class StyleSheetHandle(gdb.xmethod.XMethod):
-
-    def __init__(self):
-        super(StyleSheetHandle, self).__init__('mozilla::StyleSheetHandle::RefPtr')
-
-    def get_worker(self, method_name):
-        if method_name == 'operator*':
-            return StyleSheetHandleDereference()
-        elif method_name == 'operator->':
-            return StyleSheetHandleMember()
-
-
-class StyleSheetHandleDereference(gdb.xmethod.XMethodWorker):
-
-    def __init__(self):
-        super(StyleSheetHandleDereference, self).__init__()
-
-    def get_arg_types(self):
-        return None
-
-    def __call__(self, obj):
-        value = nested_get(obj, ('mHandle', 'mPtr', 'mValue'), gdb.Value(0L))
-        tmp = long(value)
-        if tmp == 0L:
-            type_ = 'mozilla::StyleSheet'
-        else:
-            if tmp & 0x1:
-                tmp = tmp - 1
-                type_ = 'mozilla::ServoStyleSheet'
-            else:
-                type_ = 'mozilla::CSSStyleSheet'
-
-        tmp = gdb.Value(tmp)
-        type_ = gdb.lookup_type(type_)
-        tmp = tmp.cast(type_.pointer())
-        return tmp.dereference()
-
-
-class StyleSheetHandleMember(gdb.xmethod.XMethodWorker):
-
-    def __init__(self):
-        super(StyleSheetHandleMember, self).__init__()
-
-    def get_arg_types(self):
-        return None
-
-    def __call__(self, obj):
-        value = nested_get(obj, ('mHandle', 'mPtr', 'mValue'), gdb.Value(0L))
-        tmp = long(value)
-        if tmp == 0L:
-            type_ = 'mozilla::StyleSheet'
-        else:
-            if tmp & 0x1:
-                tmp = tmp - 1
-                type_ = 'mozilla::ServoStyleSheet'
-            else:
-                type_ = 'mozilla::CSSStyleSheet'
-
-        tmp = gdb.Value(tmp)
-        type_ = gdb.lookup_type(type_)
-        tmp = tmp.cast(type_.pointer())
-        return tmp.dereference()
-
-
 #DereferenceCommand()
 gdb.xmethod.register_xmethod_matcher(None, NetscapePointerMatcher())
-gdb.xmethod.register_xmethod_matcher(None, StyleSheetHandleMatcher())
