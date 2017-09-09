113036a5350a408bf9387eeb34d417172620fb37
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-09-09 15:02:04 +0800

    update

--- sandbox/python/wcpan.model/setup.py
+++ sandbox/python/wcpan.model/setup.py
@@ -1,39 +1,23 @@
 import os
 import re
-from setuptools import setup, find_packages
+from setuptools import setup
 
-rel_file = lambda *args: os.path.join(os.path.dirname(os.path.abspath(__file__)), *args)
-
-def read_from(filename):
-    fp = open(filename)
-    try:
-        return fp.read()
-    finally:
-        fp.close()
-
-def get_long_description():
-    return read_from(rel_file('README.md'))
-
-def get_version():
-    data = read_from(rel_file('micromodels', '__init__.py'))
-    return re.search(r"__version__ = '([^']+)'", data).group(1)
 
 setup(
-    name='micromodels',
+    name='wcpan.model',
     description='Declarative dictionary-based model classes for Python',
-    long_description=get_long_description(),
-    version=get_version(),
-    packages=find_packages(),
-    url='https://github.com/j4mie/micromodels/',
-    author='Jamie Matthews',
-    author_email='jamie.matthews@gmail.com',
-    license='Public Domain',
-    install_requires=["PySO8601"],
+    version='1.0.0.dev1',
+    packages=[
+        'wcpan.model',
+    ],
+    url='https://github.com/legnaleurc/wcpan.model',
+    author='Wei-Cheng Pan',
+    author_email='legnaleurc@gmail.com',
     classifiers = [
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.3',
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: Public Domain',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
