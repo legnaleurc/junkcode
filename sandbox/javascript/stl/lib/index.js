f3e557450a932c0eb34c34229dc82e4ed0bc945e
Wei-Cheng Pan <wpan@mozilla.com>
2017-09-20 18:20:36 +0800

    update

---
+++ sandbox/javascript/stl/lib/index.js
@@ -0,0 +1,7 @@
+import {
+  map,
+} from '../src/sync';
+
+export {
+  map,
+};
---
+++ sandbox/javascript/stl/test/sync.js
@@ -0,0 +1,23 @@
+import chai from 'chai';
+
+import {
+  map,
+} from '../lib/index';
+
+chai.should();
+
+
+describe('sync', () => {
+
+  describe('map', () => {
+
+    it('should accept Array', () => {
+      let a = [1, 2, 3];
+      a = map(a, v => v * 2);
+      a = Array.from(a);
+      a.should.equal.to([2, 4, 6]);
+    });
+
+  });
+
+});
