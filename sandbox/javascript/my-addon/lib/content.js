3e38434986577421540d7435ae806684d0500dfc
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-10-06 14:45:39 +0800

    update

--- sandbox/javascript/my-addon/lib/content.js
+++ sandbox/javascript/my-addon/lib/content.js
@@ -5,20 +5,64 @@ function wait (msDelay) {
 }
 
 
-async function showPrompt (message) {
-  const position = document.activeElement.getBoundingClientRect();
-
+function makePrompt (message) {
   const block = document.createElement('div');
   block.classList.add('download-with-transmission', 'bubble');
   block.textContent = message;
+  return block;
+}
 
+
+async function showPrompt (message) {
+  const anchor = document.activeElement;
+  const block = makePrompt(message);
   document.body.appendChild(block);
 
-  block.style.top = position.top;
-  block.style.left = position.left;
+  const position = getTargetPosition(anchor, block);
+  moveElementCenterTo(block, position);
 
   // await wait(5000);
   // block.parentElement.removeChild(block);
+}
+
+
+function getTargetPosition (origin, block) {
+  origin = toCenterFromTopLeft(origin.getBoundingClientRect());
+  block = toCenterFromTopLeft(block.getBoundingClientRect());
+  // TODO top only for now
+  return {
+    x: origin.x,
+    y: origin.y - origin.height / 2 - block.height / 2,
+    width: block.width,
+    height: block.height,
+  };
+}
+
+
+function toCenterFromTopLeft (rect) {
+  return {
+    x: rect.left + rect.width / 2,
+    y: rect.top + rect.height / 2,
+    width: rect.width,
+    height: rect.height,
+  };
+}
+
+
+function toTopLeftFromCenter (rect) {
+  return {
+    left: rect.x - rect.width / 2,
+    top: rect.y - rect.height / 2,
+    width: rect.width,
+    height: rect.height,
+  };
+}
+
+
+function moveElementCenterTo (element, position) {
+  position = toTopLeftFromCenter(position);
+  element.style.top = position.top + 'px';
+  element.style.left = position.left + 'px';
 }
 
 
