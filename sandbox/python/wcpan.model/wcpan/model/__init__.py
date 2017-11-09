d6df9f2fbfb5e8e226e2ebcc02f9589873e0b91e
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-11-09 13:59:50 +0800

    remove

--- sandbox/python/wcpan.model/wcpan/model/__init__.py
+++
@@ -1,6 +0,0 @@
-from .models import Model
-from .fields import (BaseField, StringField, IntegerField, FloatField,
-                    BooleanField, DateTimeField, DateField, TimeField,
-                    ModelField, ModelCollectionField, FieldCollectionField)
-
-__version__ = '0.6.0'
