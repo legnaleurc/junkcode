2f01fd77f938a4358095440e109d037df52a3604
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-12-30 19:24:28 +0900

    update

--- sandbox/javascript/my-addon/chrome/main.js
+++ sandbox/javascript/my-addon/chrome/main.js
@@ -1,26 +1,63 @@
-browser.menus.create({
-  id: 'send-to-transmission',
-  title: 'Send To Transmission',
-  contexts: ['link'],
-});
-
-browser.menus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'send-to-transmission') {
-    main(info.linkUrl, tab.id);
-  }
-});
+const REQUEST_FILTERS = [
+  'main_frame',
+  'sub_frame',
+  // 'script',
+];
+const EMPTY_SCRIPT = '// __CENSORED__';
 
 
-async function main (linkUrl, tabId) {
-  const prompt = new PromptServer(tabId);
-  await prompt.setMessage('Sending ...');
-  let msg = 'Done';
-  try {
-    await sendToTransmission(linkUrl);
-  } catch (e) {
-    console.error('Download with Transmission', e);
-    msg = e.toString();
-  }
-  await prompt.setMessage(msg);
-  await prompt.close();
+async function main () {
+  await browser.webRequest.handlerBehaviorChanged();
+
+  browser.webRequest.onBeforeRequest.addListener(before, {
+    urls: ['<all_urls>'],
+    types: REQUEST_FILTERS,
+  }, ['blocking']);
 }
+
+
+function before (details) {
+  let isScript = details.type === 'script';
+  let filter = browser.webRequest.filterResponseData(details.requestId);
+  let decoder = new TextDecoder("utf-8");
+  let encoder = new TextEncoder();
+
+  filter.ondata = (event) => {
+    let content = decoder.decode(event.data, {
+      stream: true,
+    });
+    if (isScript) {
+      content = filterScript(content);
+    } else {
+      content = filterDocument(content);
+    }
+    content = encoder.encode(content);
+    filter.write(content);
+    filter.disconnect();
+  };
+}
+
+
+function filterScript (usScript) {
+  let pattern = /setInterval\(function\(\)\{_0x\w+\(\);\},0xfa0\)/;
+  if (pattern.test(usScript)) {
+    return EMPTY_SCRIPT;
+  }
+  return usScript;
+}
+
+
+function filterDocument (usDocument) {
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(usDocument, 'text/html');
+  let scripts = doc.querySelectorAll('script');
+  for (script of scripts) {
+    script.textContent = filterScript(script.textContent);
+  }
+  return '<!DOCTYPE html>' + doc.documentElement.outerHTML;
+}
+
+
+main().catch((e) => {
+  console.warn('main fatal', e);
+});
