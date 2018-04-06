b8d5e0ff31567e542e89ee50a425b9f553b0f247
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-04-07 08:04:08 +0900

    update

--- sandbox/cplusplus/detect_archive/detect_archive.cpp
+++ sandbox/cplusplus/detect_archive/detect_archive.cpp
@@ -3,13 +3,15 @@
 #include <string>
 #include <sstream>
 #include <memory>
+#include <vector>
 #include <cassert>
 
 #include <uchardet.h>
 
 
-std::string guess_codec(const std::string & text);
-std::string read_file_names(const std::string & file_name);
+std::string guess_codec(const std::vector<std::string> & text_list);
+std::string guess_codec_2(const std::vector<std::string> & text_list);
+std::vector<std::string> read_file_names(const std::string & file_name);
 std::string read_local_file(std::istream & is);
 uint32_t read_u32(std::istream & is);
 uint16_t read_u16(std::istream & is);
@@ -21,16 +23,50 @@ int main (int argc, char * argv[]) {
         return 1;
     }
 
-    auto text = read_file_names(argv[1]);
-    text = guess_codec(text);
+    auto text_list = read_file_names(argv[1]);
+    auto text = guess_codec(text_list);
+    std::cout << text << std::endl;
+    text = guess_codec_2(text_list);
     std::cout << text << std::endl;
 
     return 0;
 }
 
 
-std::string guess_codec(const std::string & text) {
+std::string guess_codec(const std::vector<std::string> & text_list) {
     using CDSP = std::shared_ptr<std::remove_pointer<uchardet_t>::type>;
+
+    CDSP ucd(uchardet_new(), uchardet_delete);
+    if (!ucd) {
+        return "cannot alloc uchardet";
+    }
+    for (const auto & text : text_list) {
+        int rv = uchardet_handle_data(ucd.get(), text.c_str(), text.size());
+        if (rv != 0) {
+            return "cannot handle text";
+        }
+        rv = uchardet_handle_data(ucd.get(), "\n", 1);
+        if (rv != 0) {
+            return "cannot handle text";
+        }
+    }
+    uchardet_data_end(ucd.get());
+    const char * name = uchardet_get_charset(ucd.get());
+    if (!name) {
+        return "name is null";
+    }
+    return name;
+}
+
+
+std::string guess_codec_2(const std::vector<std::string> & text_list) {
+    using CDSP = std::shared_ptr<std::remove_pointer<uchardet_t>::type>;
+
+    std::ostringstream sout;
+    for (const auto & text : text_list) {
+        sout << text << '\n';
+    }
+    std::string text = sout.str();
 
     CDSP ucd(uchardet_new(), uchardet_delete);
     if (!ucd) {
@@ -49,20 +85,20 @@ std::string guess_codec(const std::string & text) {
 }
 
 
-std::string read_file_names(const std::string & file_name) {
-    std::ostringstream sout;
+std::vector<std::string> read_file_names(const std::string & file_name) {
+    std::vector<std::string> list;
     std::ifstream fin(file_name, std::ios::binary);
     while (true) {
         uint32_t signature = read_u32(fin);
         if (signature == 0x04034b50) {
             auto file_name = read_local_file(fin);
-            sout << file_name << '\n';
+            list.push_back(file_name);
             continue;
         }
         break;
     }
     fin.close();
-    return sout.str();
+    return list;
 }
 
 
