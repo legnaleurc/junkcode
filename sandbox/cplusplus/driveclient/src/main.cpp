d74ffc8a9075a4cbe65dd2d22ccc5dacfff0617d
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-08-26 18:22:38 +0900

    update

--- sandbox/cplusplus/driveclient/src/main.cpp
+++ sandbox/cplusplus/driveclient/src/main.cpp
@@ -1,12 +1,16 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QTreeView>
 
+#include "drivemodel.h"
+
 
 int main(int argc, char * argv[]) {
     QApplication app(argc, argv);
 
+    DriveModel model;
     QTreeView view;
     view.show();
+    view.setModel(&model);
 
     return app.exec();
 }
