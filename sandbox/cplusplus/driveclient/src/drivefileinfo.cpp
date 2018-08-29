52d31acc9e80fe2902bb32a450411137d6eb9c42
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-08-29 13:21:03 +0900

    update

--- sandbox/cplusplus/driveclient/src/drivefileinfo.cpp
+++ sandbox/cplusplus/driveclient/src/drivefileinfo.cpp
@@ -1,7 +1,5 @@
 #include "drivefileinfo_p.h"
 
-#include <QtCore/QDir>
-
 
 DriveFileInfo::DriveFileInfo()
     : d_ptr(new DriveFileInfoPrivate(this))
@@ -10,27 +8,17 @@ DriveFileInfo::DriveFileInfo()
 
 DriveFileInfo::DriveFileInfo(const QString & path)
     : d_ptr(new DriveFileInfoPrivate(this, path))
-{
-    Q_D(DriveFileInfo);
-    d->fileInfo.setFile(path);
-}
+{}
 
 
 DriveFileInfo::DriveFileInfo(const QString & parentPath, const QString & name)
     : d_ptr(new DriveFileInfoPrivate(this, parentPath, name))
-{
-    Q_D(DriveFileInfo);
-    QDir parent(parentPath);
-    d->fileInfo = QFileInfo(parent, name);
-}
+{}
 
 
 DriveFileInfo::DriveFileInfo(const QFileInfo & fileInfo)
     : d_ptr(new DriveFileInfoPrivate(this, fileInfo))
-{
-    Q_D(DriveFileInfo);
-    d->fileInfo = fileInfo;
-}
+{}
 
 
 DriveFileInfo & DriveFileInfo::operator = (const QFileInfo & fileInfo) {
