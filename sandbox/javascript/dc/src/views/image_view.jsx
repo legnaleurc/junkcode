094817e2f25bde4af8af964535c4def6793d6c4b
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-11-30 10:38:45 +0900

    update

--- sandbox/javascript/dc/src/views/image_view.jsx
+++ sandbox/javascript/dc/src/views/image_view.jsx
@@ -9,14 +9,23 @@ class ImageView extends React.Component {
     super(props);
 
     this._root = React.createRef();
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: buildThresholdList(),
+    };
+    this._observer = new IntersectionObserver(this._handleIntersect.bind(this), options);
+    this._prevRatio = 0.0;
+    this._increasingColor = 'rgba(40, 40, 190, ratio)';
+    this._decreasingColor = 'rgba(190, 40, 40, ratio)';
   }
 
   componentDidMount () {
-    console.info('componentDidMount', this._root);
+    this._observer.observe(this._root.current);
   }
 
   componentWillUnmount () {
-    console.info('componentWillUnmount', this._root);
+    this._observer.disconnect();
   }
 
   render () {
@@ -25,6 +34,33 @@ class ImageView extends React.Component {
     );
   }
 
+
+  _handleIntersect (entries) {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > this._prevRatio) {
+        entry.target.style.backgroundColor = this._increasingColor.replace("ratio", entry.intersectionRatio);
+      } else {
+        entry.target.style.backgroundColor = this._decreasingColor.replace("ratio", entry.intersectionRatio);
+      }
+
+      this._prevRatio = entry.intersectionRatio;
+    });
+  }
+
+}
+
+
+function buildThresholdList () {
+  const thresholds = [];
+  const numSteps = 20;
+
+  for (let i = 1.0; i <= numSteps; ++i) {
+    const ratio = i / numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
 }
 
 
