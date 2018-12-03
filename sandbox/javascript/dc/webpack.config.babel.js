0667db541738fec1bc45cbaa4fb36687231a9313
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-12-03 13:26:36 +0900

    add production
    babel-runtime just for cdn

--- sandbox/javascript/dc/webpack.config.babel.js
+++ sandbox/javascript/dc/webpack.config.babel.js
@@ -1,4 +1,13 @@
+import process from 'process';
+
 import HtmlWebPackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+import DynamicCdnWebpackPlugin from 'dynamic-cdn-webpack-plugin';
+
+
+const devMode = process.env.NODE_ENV !== 'production'
 
 
 export default {
@@ -24,8 +33,9 @@ export default {
       {
         test: /\.s?css$/,
         use: [
-          'style-loader',
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
           'css-loader',
+          'postcss-loader',
           'sass-loader',
         ],
       },
@@ -40,6 +50,24 @@ export default {
       // this uses the path related to output directory, not source directory
       filename: 'index.html',
     }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? '[name].css' : '[name].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+    }),
+    new DynamicCdnWebpackPlugin(),
   ],
   devtool: 'inline-source-map',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 6,
+        },
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
 };
