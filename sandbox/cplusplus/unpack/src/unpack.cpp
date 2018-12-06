c30efe52c1675b7422c60592dc4dbf13ab5904d7
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-12-06 09:44:13 +0900

    update

--- sandbox/cplusplus/unpack/src/unpack.cpp
+++ sandbox/cplusplus/unpack/src/unpack.cpp
@@ -1,9 +1,9 @@
-#include "unpack.h"
+#include "unpack.hpp"
 
 #include <archive.h>
 
 
-int unpack_to(int port, const char * http_path, const char * output_path) {
+void unpack_to(const std::string & url, const std::string & path) {
     int rv = 0;
     struct archive * handle = archive_read_new();
     rv = archive_read_free(handle);
---
+++ sandbox/cplusplus/unpack/src/unpack.hpp
@@ -0,0 +1,6 @@
+#ifndef UNPACK_HPP
+#define UNPACK_HPP
+
+void unpack_to(const std::string & url, const std::string & path);
+
+#endif
