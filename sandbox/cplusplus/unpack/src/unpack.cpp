a69d99f076e384f0217d9188583426121ae3511e
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-12-06 13:40:13 +0900

    update

--- sandbox/cplusplus/unpack/src/unpack.cpp
+++ sandbox/cplusplus/unpack/src/unpack.cpp
@@ -7,10 +7,19 @@
 #include <cassert>
 
 
+struct Context {
+    std::string url;
+    std::string path;
+};
+
+
+using ContextHandle = std::shared_ptr<Context>;
 using ArchiveHandle = std::shared_ptr<struct archive>;
 
 
-ArchiveHandle createArchiveHandle ();
+ArchiveHandle createArchiveReader (ContextHandle context);
+ArchiveHandle createDiskWriter ();
+void extractArchive(ArchiveHandle reader, ArchiveHandle writer);
 
 int open_callback (struct archive * handle, void * context);
 int	close_callback (struct archive * handle, void * context);
@@ -22,11 +31,31 @@ la_int64_t seek_callback (struct archive * handle, void * context,
 
 void unpack_to (const std::string & url, const std::string & path) {
     int rv = 0;
-    auto handle = createArchiveHandle();
+    ContextHandle context = std::make_shared<Context>();
+    auto reader = createArchiveReader(context);
+    auto writer = createDiskWriter();
+
+    for (;;) {
+        struct archive_entry * entry = nullptr;
+        int rv = archive_read_next_header(reader.get(), &entry);
+
+        if (rv == ARCHIVE_EOF) {
+            break;
+        }
+        assert(rv == ARCHIVE_OK || !"archive_read_next_header");
+
+        rv = archive_write_header(writer.get(), entry);
+        assert(rv == ARCHIVE_OK || !"archive_write_header");
+
+        extractArchive(reader, writer);
+
+        rv = archive_write_finish_entry(writer.get());
+        assert(rv == ARCHIVE_OK || !"archive_write_finish_entry");
+    }
 }
 
 
-ArchiveHandle createArchiveHandle () {
+ArchiveHandle createArchiveReader (ContextHandle context) {
     ArchiveHandle handle(archive_read_new(), [](ArchiveHandle::element_type * p) -> void {
         archive_read_free(p);
     });
@@ -39,7 +68,16 @@ ArchiveHandle createArchiveHandle () {
     archive_read_set_read_callback(handle.get(), read_callback);
     archive_read_set_seek_callback(handle.get(), seek_callback);
 
-    archive_read_set_callback_data(handle.get(), NULL);
+    archive_read_set_callback_data(handle.get(), context.get());
+
+    return handle;
+}
+
+
+ArchiveHandle createDiskWriter () {
+    ArchiveHandle handle(archive_write_disk_new(), [](ArchiveHandle::element_type * p) -> void {
+        archive_write_free(p);
+    });
 
     return handle;
 }
