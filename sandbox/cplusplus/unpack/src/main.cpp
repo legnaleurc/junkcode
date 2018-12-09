dcf506f4ffeda5cd951d816af2a10e1bf48f96cf
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-12-09 12:23:59 +0900

    update

--- sandbox/cplusplus/unpack/src/main.cpp
+++ sandbox/cplusplus/unpack/src/main.cpp
@@ -1,10 +1,19 @@
 #include "unpack.hpp"
 
+#ifndef _WIN32
+#include <locale.h>
+#endif
+
 
 int main (int argc, char * argv[]) {
     if (argc != 4) {
         return 1;
     }
+
+    // libarchive/libarchive#587
+#ifndef _WIN32
+    setlocale(LC_ALL, "");
+#endif
 
     auto port = std::stoul(argv[1]);
     unpackTo(port, argv[2], argv[3]);
