2e0c591f5d9b6a34aaece157ddddf3e85dd5bcdd
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-12-11 16:44:24 +0900

    update

---
+++ sandbox/javascript/dc/src/views/sort_action_bar.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Button from './button';
+import { setSortFunction } from '../states/file_system/actions';
+import {
+  SORT_BY_NAME_ASC,
+  SORT_BY_MTIME_ASC,
+  SORT_BY_MTIME_DES,
+} from '../states/file_system/sort';
+
+
+class SortActionBar extends React.Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    const { setSortFunction } = this.props;
+    return (
+      <div className="content action-bar">
+        <div className="group">
+          <Button
+            onClick={event => {
+              setSortFunction(SORT_BY_NAME_ASC);
+            }}
+          >
+            SORT_BY_NAME_ASC
+          </Button>
+          <Button
+            onClick={event => {
+              setSortFunction(SORT_BY_MTIME_ASC);
+            }}
+          >
+            SORT_BY_MTIME_ASC
+          </Button>
+          <Button
+            onClick={event => {
+              setSortFunction(SORT_BY_MTIME_DES);
+            }}
+          >
+            SORT_BY_MTIME_DES
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
+function mapDispatchToProps (dispatch) {
+  return {
+    setSortFunction (key) {
+      dispatch(setSortFunction(key));
+    },
+  };
+}
+
+
+export default connect(null, mapDispatchToProps)(SortActionBar);
