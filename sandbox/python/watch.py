11f345377608c5aa83746816ad2fd1fa91123415
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-01-25 16:01:38 +0900

    update

--- sandbox/python/watch.py
+++ sandbox/python/watch.py
@@ -13,7 +13,18 @@ import signal
 
 class Watcher(object):
 
-    def __init__(self, executor=None, loop=None):
+    def __init__(self,
+        stop_event=None,
+        min_sleep=50,
+        normal_sleep=400,
+        debounce=1600,
+        executor=None,
+        loop=None,
+    ):
+        self._stop_event = stop_event
+        self._min_sleep = min_sleep
+        self._normal_sleep = normal_sleep
+        self._debounce = debounce
         self._executor = executor
         self._loop = loop
         if not self._loop:
@@ -24,7 +35,7 @@ class Watcher(object):
             if self._executor is None:
                 self._executor = stack.enter_context(cf.ThreadPoolExecutor())
             self._raii = stack.pop_all()
-        return WatcherCreator(self._run)
+        return WatcherCreator(self)
 
     async def __aexit__(self, type_, exc, tb):
         await self._raii.aclose()
@@ -33,26 +44,60 @@ class Watcher(object):
         fn = ft.partial(cb, *args, **kwargs)
         return await self._loop.run_in_executor(self._executor, fn)
 
+    async def _sleep(self, sec):
+        await aio.sleep(sec, loop=self._loop)
+
 
 class WatcherCreator(object):
 
-    def __init__(self, runner):
-        self._runner = runner
+    def __init__(self, context):
+        self._context = context
 
-    def __call__(self, path, stop_event=None):
-        return ChangeIterator(self._runner, path, stop_event)
+    def __call__(self,
+        path,
+        stop_event=None,
+        min_sleep=None,
+        normal_sleep=None,
+        debounce=None,
+    ):
+        if stop_event is None:
+            stop_event = self._context._stop_event
+        if min_sleep is None:
+            min_sleep = self._context._min_sleep
+        if normal_sleep is None:
+            normal_sleep = self._context._normal_sleep
+        if debounce is None:
+            debounce = self._context._debounce
+
+        return ChangeIterator(
+            run=self._context._run,
+            sleep=self._context._sleep,
+            path=path,
+            stop_event=stop_event,
+            min_sleep=min_sleep,
+            normal_sleep=normal_sleep,
+            debounce=debounce,
+        )
 
 
 class ChangeIterator(object):
 
-    def __init__(self, runner, path, stop_event):
-        self._runner = runner
+    def __init__(self,
+        run,
+        sleep,
+        path,
+        stop_event,
+        min_sleep,
+        normal_sleep,
+        debounce,
+    ):
+        self._run = run
+        self._sleep = sleep
         self._path = path
         self._stop_event = stop_event
-
-        self._min_sleep = 50
-        self._normal_sleep = 400
-        self._debounce = 1600
+        self._min_sleep = min_sleep
+        self._normal_sleep = normal_sleep
+        self._debounce = debounce
 
     def __aiter__(self):
         return self
@@ -73,10 +118,10 @@ class ChangeIterator(object):
                     sleep_time = self._min_sleep
                 else:
                     sleep_time = max(self._normal_sleep - check_time, self._min_sleep)
-                await aio.sleep(sleep_time / 1000)
+                await self._sleep(sleep_time / 1000)
 
             s = unix_ms()
-            new_changes = await self._runner(lambda: [123])
+            new_changes = await self._run(lambda: [123])
             changes.update(new_changes)
 
             now = unix_ms()
