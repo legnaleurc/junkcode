396ff0a556a0464d320cb2737caa230b9e54fd2a
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-04-15 16:52:35 +0900

    update

--- algorithm/leetcode/406.cpp
+++ algorithm/leetcode/406.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
         using P = pair<int, int>;
-        
+
         sort(begin(people), end(people), [](const P & l, const P & r) -> bool {
             if (l.first > r.first) {
                 return true;
@@ -12,14 +12,14 @@ public:
             }
             return l.second < r.second;
         });
-        
+
         list<P> rv;
         for (const auto & p: people) {
             auto it = begin(rv);
             advance(it, p.second);
             rv.insert(it, p);
         }
-        
+
         return { begin(rv), end(rv) };
     }
 };
