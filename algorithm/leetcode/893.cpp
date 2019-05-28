c3a46497d4a5fd8a867a5964d2cfa6f9c2c53aa4
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-05-28 11:26:55 +0900

    update

---
+++ algorithm/leetcode/893.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        return leftMost(root);
+    }
+    
+    TreeNode* leftMost(TreeNode* node) {
+        if (!node) {
+            return nullptr;
+        }
+        auto root = node;
+        if (node->left) {
+            auto left = leftMost(node->left);
+            root = left;
+            while (left->right) {
+                left = left->right;
+            }
+            left->right = node;
+            node->left = nullptr;
+        }
+        if (node->right) {
+            auto right = leftMost(node->right);
+            node->right = right;
+        }
+        return root;
+    }
+};
