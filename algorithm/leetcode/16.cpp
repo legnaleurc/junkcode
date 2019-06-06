dac83927553f1ed0c96d8eb280df0511e8a5d32e
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-06-07 00:37:51 +0900

    add

---
+++ algorithm/leetcode/16.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        
+        int rv = nums[0] + nums[1] + nums[2];
+        
+        for (int i = 0; i < nums.size() - 2; ++i) {
+            int goal = target - nums[i];
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (abs(sum - goal) < abs(rv - target)) {
+                    rv = nums[i] + sum;
+                }
+                
+                if (sum < goal) {
+                    ++left;
+                } else if (sum > goal) {
+                    --right;
+                } else {
+                    return target;
+                }
+            }
+        }
+        
+        return rv;
+    }
+};
---
+++ algorithm/leetcode/295.cpp
@@ -0,0 +1,59 @@
+class MedianFinder {
+public:
+    /** initialize your data structure here. */
+    MedianFinder() {
+        this->median = 0.0;
+    }
+    
+    void addNum(int num) {
+        if (this->small.empty() && this->big.empty()) {
+            this->median = num;
+            this->small.push(num);
+            return;
+        }
+        
+        if (this->small.size() > this->big.size()) {
+            if (num > this->median) {
+                this->big.push(num);
+            } else {
+                this->big.push(this->small.top());
+                this->small.pop();
+                this->small.push(num);
+            }
+            this->median = (this->small.top() + this->big.top()) / 2.0;
+        } else if (this->small.size() < this->big.size()) {
+            if (num < this->median) {
+                this->small.push(num);
+            } else {
+                this->small.push(this->big.top());
+                this->big.pop();
+                this->big.push(num);
+            }
+            this->median = (this->small.top() + this->big.top()) / 2.0;
+        } else {
+            if (num > this->median) {
+                this->big.push(num);
+                this->median = this->big.top();
+            } else {
+                this->small.push(num);
+                this->median = this->small.top();
+            }
+        }
+    }
+    
+    double findMedian() {
+        return this->median;
+    }
+    
+private:
+    double median;
+    priority_queue<double> small;
+    priority_queue<double, vector<double>, greater<double>> big;
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
