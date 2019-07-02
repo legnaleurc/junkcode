43edd6be636029d0965a131e8d845cb82646debd
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-07-03 02:08:36 +0900

    update

--- algorithm/leetcode/39.cpp
+++ algorithm/leetcode/39.cpp
@@ -4,28 +4,29 @@ public:
         sort(begin(candidates), end(candidates));
 
         vector<vector<int>> rv;
-        for (int i = 0; i < candidates.size(); ++i) {
-            findSum(rv, target, candidates, i, {}, 0);
-        }
+        vector<int> v;
+        findSum(rv, target, candidates, 0, v, 0);
 
         return rv;
     }
     
-    void findSum(vector<vector<int>>& rv, int target, const vector<int>& candidates, int i, vector<int> v, int sum) {
-        if (i >= candidates.size()) {
-            return;
-        }
-        sum += candidates[i];
-        if (sum > target) {
-            return;
-        }
-        v.push_back(candidates[i]);
+    void findSum(vector<vector<int>>& rv, int target, const vector<int>& candidates, int i, vector<int>& v, int sum) {
         if (sum == target) {
             rv.push_back(v);
             return;
         }
+        if (i >= candidates.size()) {
+            return;
+        }
         for (; i < candidates.size(); ++i) {
+            sum += candidates[i];
+            if (sum > target) {
+                return;
+            }
+            v.push_back(candidates[i]);
             findSum(rv, target, candidates, i, v, sum);
+            sum -= candidates[i];
+            v.pop_back();
         }
     }
 };
