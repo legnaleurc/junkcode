d099da4ef72192a9d1f4b7622628135cd1c61c1b
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-07-29 16:43:27 +0900

    update

--- algorithm/leetcode/21.cpp
+++ algorithm/leetcode/21.cpp
@@ -9,32 +9,21 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1 && !l2) {
+            return nullptr;
+        }
         if (!l1) {
             return l2;
         }
         if (!l2) {
             return l1;
         }
-        if (l1->val == l2->val) {
-            auto nextL1 = l1->next;
-            auto nextL2 = l2->next;
-            auto head = l1;
-            auto tail = l2;
-            l1->next = l2;
-            tail->next = mergeTwoLists(nextL1, nextL2);
-            return head;
-        }
         if (l1->val < l2->val) {
-            auto head = l1;
-            auto nextL1 = l1->next;
-            head->next = mergeTwoLists(nextL1, l2);
-            return head;
-        }
-        if (l1->val > l2->val) {
-            auto head = l2;
-            auto nextL2 = l2->next;
-            head->next = mergeTwoLists(l1, nextL2);
-            return head;
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
         }
         return nullptr;
     }
