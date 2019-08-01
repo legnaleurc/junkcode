0a83db4080fa329c8bd4d28d9acbbbd1a1033f71
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-08-01 11:42:50 +0900

    update

--- algorithm/leetcode/221.cpp
+++ algorithm/leetcode/221.cpp
@@ -16,9 +16,6 @@ public:
                 if (matrix[row][col] == 0) {
                     continue;
                 }
-                if (matrix[row-1][col] == 0 || matrix[row][col-1] == 0 || matrix[row-1][col-1] == 0) {
-                    continue;
-                }
                 matrix[row][col] = min({ matrix[row-1][col], matrix[row][col-1], matrix[row-1][col-1] }) + 1;
             }
         }
