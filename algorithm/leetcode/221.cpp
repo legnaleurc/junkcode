3ae1db51442c725364851c79dcc65937afeefdd8
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-08-01 11:39:30 +0900

    add

---
+++ algorithm/leetcode/221.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        for (auto & row : matrix) {
+            for (auto & col : row) {
+                if (col == '0') {
+                    col = 0;
+                } else {
+                    col = 1;
+                }
+            }
+        }
+
+        for (int row = 1; row < matrix.size(); ++row) {
+            for (int col = 1; col < matrix[row].size(); ++col) {
+                if (matrix[row][col] == 0) {
+                    continue;
+                }
+                if (matrix[row-1][col] == 0 || matrix[row][col-1] == 0 || matrix[row-1][col-1] == 0) {
+                    continue;
+                }
+                matrix[row][col] = min({ matrix[row-1][col], matrix[row][col-1], matrix[row-1][col-1] }) + 1;
+            }
+        }
+
+        int rv = 0;
+        for (auto & row : matrix) {
+            for (auto & col : row) {
+                if (col > rv) {
+                    rv = col;
+                }
+            }
+        }
+
+        return rv * rv;
+    }
+};
