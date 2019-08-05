4d2edabe3cee362baaa27c8e8464ac9adfb9358c
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-08-05 13:38:01 +0900

    update

--- algorithm/leetcode/48.cpp
+++ algorithm/leetcode/48.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        for (auto & row : matrix) {
-            reverse(begin(row), end(row));
-        }
+        reverse(begin(matrix), end(matrix));
         for (int row = 0; row < matrix.size(); ++row) {
-            const auto size = matrix.size() - row;
-            for (int col = 0; col < size; ++col) {
-                const auto offset = matrix.size() - 1;
-                swap(matrix[row][col], matrix[offset - col][offset - row]);
+            for (int col = 0; col < row; ++col) {
+                swap(matrix[row][col], matrix[col][row]);
             }
         }
     }
