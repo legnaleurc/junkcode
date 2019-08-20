42768a66dc886b3f95aa285b1dcf9d169822ee8c
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-08-20 17:08:40 +0900

    update

--- algorithm/leetcode/338.cpp
+++ algorithm/leetcode/338.cpp
@@ -1,33 +1,10 @@
 class Solution {
-    unordered_map<uint8_t, int> cache;
 public:
     vector<int> countBits(int num) {
-        vector<int> rv;
-        for (int i = 0; i <= num; ++i) {
-            rv.push_back(count(i));
+        vector<int> rv{{ 0 }};
+        for (int i = 1; i <= num; ++i) {
+            rv.push_back(rv[i >> 1] + (i & 0x1));
         }
         return rv;
-    }
-
-    int count(int n) {
-        uint8_t * bits = reinterpret_cast<uint8_t *>(&n);
-        int sum = 0;
-        for (int i = 0; i < sizeof(int); ++i) {
-            auto it = cache.find(bits[i]);
-            if (it != end(cache)) {
-                sum += it->second;
-                continue;
-            }
-
-            int cc = 0;
-            uint8_t c = bits[i];
-            while (c) {
-                cc += c & 0x01;
-                c = c >> 1;
-            }
-            cache.insert({ bits[i], cc });
-            sum += cc;
-        }
-        return sum;
     }
 };
