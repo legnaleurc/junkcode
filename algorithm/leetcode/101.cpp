6c13091ee11ea8a441ead1d071498effc7446849
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-08-23 17:48:28 +0900

    add

---
+++ algorithm/leetcode/101.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+        return isSymmetric(root->left, root->right);
+    }
+
+    bool isSymmetric(TreeNode* left, TreeNode* right) {
+        if (!left && !right) {
+            return true;
+        }
+        if (!left || !right) {
+            return false;
+        }
+        if (left->val != right->val) {
+            return false;
+        }
+        bool outer = isSymmetric(left->left, right->right);
+        bool inner = isSymmetric(left->right, right->left);
+        return outer && inner;
+    }
+};
