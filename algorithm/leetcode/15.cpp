e6861aba68844e5908297c63b5fd7a8f008656c7
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-08-30 10:21:36 +0900

    fix

--- algorithm/leetcode/15.cpp
+++ algorithm/leetcode/15.cpp
@@ -4,35 +4,31 @@ public:
         if (nums.size() < 3) {
             return {};
         }
-        sort(nums.begin(), nums.end());
-
+        sort(begin(nums), end(nums));
         vector<vector<int>> rv;
         for (int i = 0; i < nums.size() - 2; ++i) {
             if (i > 0 && nums[i] == nums[i-1]) {
                 continue;
             }
 
-            int left = i + 1;
-            int right = nums.size() - 1;
-            while (left < right) {
-                int two = nums[left] + nums[right];
-                int one = 0 - nums[i];
-                if (two == one) {
-                    rv.push_back({nums[i], nums[left], nums[right]});
-
-                    ++left;
-                    while (nums[left] == nums[left-1]) {
-                        ++left;
-                    }
-
-                    --right;
-                    while (nums[right] == nums[right+1]) {
-                        --right;
-                    }
-                } else if (two < one) {
-                    ++left;
+            int l = i + 1;
+            int r = nums.size() - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum < 0) {
+                    ++l;
+                } else if (sum > 0) {
+                    --r;
                 } else {
-                    --right;
+                    rv.push_back({ nums[i], nums[l], nums[r] });
+                    ++l;
+                    while (l < r && nums[l] == nums[l-1]) {
+                        ++l;
+                    }
+                    --r;
+                    while (l < r && nums[r] == nums[r+1]) {
+                        --r;
+                    }
                 }
             }
         }
