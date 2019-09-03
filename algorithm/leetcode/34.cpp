1a2407f45fe69ae7d8045efa532c34b728bc2f21
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-09-03 15:07:13 +0900

    update

--- algorithm/leetcode/34.cpp
+++ algorithm/leetcode/34.cpp
@@ -1,10 +1,45 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        auto p = equal_range(begin(nums), end(nums), target);
-        if (p.first == p.second) {
+        auto b = lowerBound(nums, 0, nums.size(), target);
+        auto e = upperBound(nums, 0, nums.size(), target);
+        if (b == e) {
             return { -1, -1 };
         }
-        return { distance(begin(nums), p.first), distance(begin(nums), p.second) - 1 };
+        return { b, e - 1 };
+    }
+
+    int lowerBound(const vector<int>& nums, int b, int e, int target) const {
+        int d = e - b;
+        int step = 0;
+        while (d > 0) {
+            int i = b;
+            step = d / 2;
+            i += step;
+            if (nums[i] < target) {
+                b = ++i;
+                d -= step + 1;
+            } else {
+                d = step;
+            }
+        }
+        return b;
+    }
+
+    int upperBound(const vector<int>& nums, int b, int e, int target) const {
+        int d = e - b;
+        int step = 0;
+        while (d > 0) {
+            int i = b;
+            step = d / 2;
+            i += step;
+            if (nums[i] <= target) {
+                b = ++i;
+                d -= step + 1;
+            } else {
+                d = step;
+            }
+        }
+        return b;
     }
 };
