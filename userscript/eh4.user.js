22c4291858d2641002e9ef311b79331216e1da09
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-09-07 19:42:06 +0900

    add

---
+++ userscript/eh4.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name     eh style
+// @version  1
+// @grant    none
+// @include  https://exhentai.org/*
+// ==/UserScript==
+
+
+function main () {
+  if (location.pathname === '/' || location.pathname.startsWith('/tag/')) {
+    modifyMainPage();
+  } else if (location.pathname.indexOf('/mpv/') === 0) {
+    modifyMultiPageViewer();
+  }
+}
+
+
+function modifyMainPage () {
+  addStyle(`
+    div.ido {
+      min-width: unset;
+      width: 98%;
+    }
+
+    div.itg {
+      min-width: unset;
+    }
+  `);
+}
+
+
+function modifyMultiPageViewer () {
+  addStyle(`
+    div.mi0 {
+      min-width: unset;
+    }
+  `);
+
+  setInterval(() => {
+    let ai = document.querySelectorAll('#pane_images_inner .mi0 > a > img');
+    ai.forEach((img) => {
+      img.style.width = '100%';
+      img.style.height = 'auto';
+    });
+  }, 2000);
+}
+
+
+function addStyle (text) {
+  let style = document.createElement('style');
+  style.type = 'text/css';
+  style.textContent = text;
+  document.head.appendChild(style);
+}
+
+
+try{
+  main();
+} catch (e) {
+  console.info(e);
+}
