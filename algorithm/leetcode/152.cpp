b9151010d339793ffc0265d35525aaf95d0fbdc7
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-09-20 14:00:35 +0900

    update

--- algorithm/leetcode/152.cpp
+++ algorithm/leetcode/152.cpp
@@ -6,9 +6,8 @@ public:
         }
         int max_ = nums[0];
         for (int b = 0; b < nums.size(); ++b) {
-            int p = nums[b];
-            max_ = max(p, max_);
-            for (int e = b + 1; e < nums.size(); ++e) {
+            int p = 1;
+            for (int e = b; e < nums.size(); ++e) {
                 p *= nums[e];
                 max_ = max(p, max_);
             }
