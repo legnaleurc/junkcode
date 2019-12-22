52cd2f2bb2b0c20fded251b48dcb76cc4f50d392
Wei-Cheng Pan <legnaleurc@gmail.com>
2019-12-22 18:57:59 +0900

    _ liajwef

--- sandbox/python/finddup.py
+++ sandbox/python/finddup.py
@@ -29,18 +29,37 @@ async def main():
         root_node = await drive.get_node_by_path(from_path)
         dup_list = []
         async for dummy_root, folders, files in drive.walk(root_node):
-            nodes = folders + files
-            seen = {}
-            for node in nodes:
-                if node.name not in seen:
-                    seen[node.name] = [node.to_dict()]
-                else:
-                    seen[node.name].append(node.to_dict())
-            for node_list in seen.values():
-                if len(node_list) > 1:
-                    dup_list.append(node_list)
+            await purge_folder(dup_list, drive, folders)
+            await purge_file(dup_list, drive, files)
 
-    yaml.safe_dump(dup_list, default_flow_style=False)
+    with open('/tmp/x.yaml', 'wb') as fout:
+        yaml.safe_dump(dup_list, stream=fout, default_flow_style=False, encoding='utf-8', allow_unicode=True)
+
+
+async def purge_folder(rv, drive, folder_list):
+    seen = {}
+    for node in folder_list:
+        if node.name not in seen:
+            seen[node.name] = [node]
+        else:
+            seen[node.name].append(node)
+    for node_list in seen.values():
+        if len(node_list) > 1:
+            rv.append([node.to_dict() for node in node_list])
+
+
+async def purge_file(rv, drive, file_list):
+    seen = {}
+    for node in file_list:
+        if node.name not in seen:
+            seen[node.name] = [node]
+        elif node.hash_ == seen[node.name][-1].hash_:
+            await drive.trash_node(node)
+        else:
+            seen[node.name].append(node)
+    for node_list in seen.values():
+        if len(node_list) > 1:
+            rv.append([node.to_dict() for node in node_list])
 
 
 if __name__ == '__main__':
