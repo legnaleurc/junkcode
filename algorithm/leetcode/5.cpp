c62bc6c6723b76df51b3f6e268da5416c3fc680b
Wei-Cheng Pan <legnaleurc@gmail.com>
2021-02-07 02:41:29 +0900

    update

--- algorithm/leetcode/5.cpp
+++ algorithm/leetcode/5.cpp
@@ -1,3 +1,32 @@
+// O(n^2)
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        auto rv = make_pair(0, 0);
+        for (int i = 0; i < s.size(); ++i) {
+            auto odd = expand(s, i, i);
+            auto even = expand(s, i, i + 1);
+            if (odd.second - odd.first > rv.second - rv.first) {
+                rv = odd;
+            }
+            if (even.second - even.first > rv.second - rv.first) {
+                rv = even;
+            }
+        }
+        return s.substr(rv.first, rv.second - rv.first);
+    }
+    
+    pair<int, int> expand(const string& s, int b, int e) {
+        while (b >= 0 && e < s.size() && s[b] == s[e]) {
+            --b;
+            ++e;
+        }
+        return {b + 1, e};
+    }
+};
+
+
+// O(n^3)
 class Solution {
 public:
     string longestPalindrome(string s) {
