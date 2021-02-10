f261ccfd35b3225fc1c737ae7641efffce3d1dca
Wei-Cheng Pan <legnaleurc@gmail.com>
2021-02-10 15:10:44 +0900

    update

--- algorithm/leetcode/141.cpp
+++ algorithm/leetcode/141.cpp
@@ -9,15 +9,17 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        set<ListNode*> cache;
-        while (head) {
-            auto it = cache.find(head);
-            if (it == end(cache)) {
-                cache.insert(head);
-            } else {
+        if (!head) {
+            return false;
+        }
+        auto slow = head;
+        auto fast = head;
+        while (slow && fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
                 return true;
             }
-            head = head->next;
         }
         return false;
     }
