ecc0b32be9d4a85fabe24427f2b3571a15f39b17
Wei-Cheng Pan <legnaleurc@gmail.com>
2021-09-13 17:07:03 +0900

    update

--- sandbox/python/migrate.py
+++ sandbox/python/migrate.py
@@ -7,7 +7,7 @@ import re
 import sqlite3
 import sys
 
-from wcpan.drive.core.drive import DriveFactory
+from wcpan.drive.core.drive import DriveFactory, Drive, Node
 from wcpan.drive.core.types import MediaInfo
 from wcpan.logger import setup as setup_logger, INFO
 
@@ -21,26 +21,34 @@ async def main():
     from_path = sys.argv[1]
     initialize_cache()
 
-    factory = DriveFactory()
-    factory.load_config()
+    src_factory = get_src_drive()
+    dst_factory = get_dst_drive()
 
-    async with factory() as drive:
-        async for change in drive.sync():
-            INFO('migrate') << change
+    async with src_factory() as src_drive,\
+               dst_factory() as dst_drive:
+        async for change in src_drive.sync():
+            INFO('migrate') << 'src sync' << change
+        async for change in dst_drive.sync():
+            INFO('migrate') << 'dst sync' << change
 
-        root_node = await drive.get_node_by_path(from_path)
-        await migrate_root(drive, root_node)
+        src_root_node = await src_drive.get_node_by_path(from_path)
+        assert src_root_node is not None, 'invalid source root'
 
-        async for root, folders, files in drive.walk(root_node):
-            root_path = await drive.get_path(root)
-            new_root_path = pathlib.PurePath(*(('/', 'new') + root_path.parts[2:]))
-            new_root = await get_node(drive, new_root_path)
-            assert new_root is not None
-            INFO('migrate') << f'working on {new_root_path}'
+        async for src_root, src_folders, src_files in src_drive.walk(src_root_node):
+            src_root_path = await src_drive.get_path(src_root)
+            dst_root = await dst_drive.get_node_by_path(src_root_path)
+            assert dst_root is not None, 'invalid destination node'
+            INFO('migrate') << f'working on {src_root_path}'
 
-            lock = asyncio.Semaphore(6)
-            folder_list = [locked_migrate_folder(lock, drive, node, new_root) for node in folders]
-            file_list = [locked_migrate_file(lock, drive, node, new_root) for node in files]
+            lock = asyncio.Semaphore(4)
+            folder_list = [
+                locked_migrate_folder(lock, src_drive, node, dst_drive, dst_root)
+                for node in src_folders
+            ]
+            file_list = [
+                locked_migrate_file(lock, src_drive, node, dst_drive, dst_root)
+                for node in src_files
+            ]
             task_list = folder_list + file_list
             await asyncio.gather(*task_list)
 
@@ -66,47 +74,46 @@ async def migrate_root(drive, node):
         parent_node = node
 
 
-async def migrate_folder(drive, node, new_root):
-    new_node = await drive.get_node_by_name_from_parent(node.name, new_root)
+async def migrate_folder(src_drive: Drive, src_node: Node, dst_drive: Drive, dst_root: Node):
+    new_node = await dst_drive.get_node_by_name_from_parent(src_node.name, dst_root)
     if new_node:
         INFO('migrate') << 'folder exists, skip'
         return new_node
 
-    new_node = await drive.create_folder(new_root, node.name, exist_ok=True)
+    new_node = await dst_drive.create_folder(dst_root, src_node.name, exist_ok=True)
     return new_node
 
 
-async def migrate_file(drive, node, new_root):
-    new_node = await drive.get_node_by_name_from_parent(node.name, new_root)
+async def migrate_file(src_drive: Drive, src_node: Node, dst_drive: Drive, dst_root: Node):
+    new_node = await dst_drive.get_node_by_name_from_parent(src_node.name, dst_root)
     if new_node:
         if is_migrated(new_node):
             INFO('migrate') << 'file exists (cached), skip'
             return new_node
-        new_hash = await get_node_hash(drive, node)
-        if new_hash == new_node.hash_:
+        if src_node.hash_ == new_node.hash_:
             set_migrated(new_node)
             INFO('migrate') << 'file exists (remote verified), skip'
             return new_node
 
-    new_hash, new_node = await copy_node(drive, node, new_root)
-    assert new_node.size == node.size
+    new_hash, new_node = await copy_node(src_drive, src_node, dst_drive, dst_root)
+    assert new_node.size == src_node.size
     assert new_hash == new_node.hash_
     set_migrated(new_node)
     return new_node
 
 
-async def locked_migrate_folder(lock, drive, node, new_root):
+async def locked_migrate_folder(lock: asyncio.Semaphore, src_drive: Drive, src_node: Node, dst_drive: Drive, dst_root: Node):
     async with lock:
-        INFO('migrate') << f'mkdir {node.name}'
-        await migrate_folder(drive, node, new_root)
-        INFO('migrate') << f'ok {node.name}'
+        INFO('migrate') << f'mkdir {src_node.name}'
+        await migrate_folder(src_drive, src_node, dst_drive, dst_root)
+        INFO('migrate') << f'ok {src_node.name}'
 
 
-async def locked_migrate_file(lock, drive, node, new_root):
+async def locked_migrate_file(lock: asyncio.Semaphore, src_drive: Drive, src_node: Node, dst_drive: Drive, dst_root: Node):
     async with lock:
-        INFO('migrate') << f'touch {node.name}'
-        await migrate_file(drive, node, new_root)
-        INFO('migrate') << f'ok {node.name}'
+        INFO('migrate') << f'touch {src_node.name}'
+        await migrate_file(src_drive, src_node, dst_drive, dst_root)
+        INFO('migrate') << f'ok {src_node.name}'
 
 
 async def get_node(drive, path):
@@ -130,23 +137,23 @@ async def get_node_hash(drive, node):
     return rv
 
 
-async def copy_node(drive, node, new_root):
-    hasher = await drive.get_hasher()
+async def copy_node(src_drive: Drive, src_node: Node, dst_drive: Drive, new_root: Node):
+    hasher = await dst_drive.get_hasher()
     media_info = None
-    if node.is_image:
-        media_info = MediaInfo.image(node.image_width, node.image_height)
-    if node.is_video:
-        media_info = MediaInfo.video(node.video_width, node.video_height, node.video_ms_duration)
+    if src_node.is_image:
+        media_info = MediaInfo.image(src_node.image_width, src_node.image_height)
+    if src_node.is_video:
+        media_info = MediaInfo.video(src_node.video_width, src_node.video_height, src_node.video_ms_duration)
 
-    async with await drive.upload(
+    async with await dst_drive.upload(
         new_root,
-        node.name,
-        file_size=node.size,
-        mime_type=node.mime_type,
+        src_node.name,
+        file_size=src_node.size,
+        mime_type=src_node.mime_type,
         media_info=media_info,
     ) as fout:
-        if node.size > 0:
-            async with await drive.download(node) as fin:
+        if src_node.size > 0:
+            async with await src_drive.download(src_node) as fin:
                 async for chunk in fin:
                     hasher.update(chunk)
                     await fout.write(chunk)
@@ -196,6 +203,36 @@ def set_migrated(node):
         query.execute('''
             INSERT INTO migrated (node_id) VALUES (?);
         ''', (node.id_,))
+
+
+def get_config_path(root: str):
+    path = pathlib.Path(f'./{root}/config')
+    path = path.expanduser()
+    path = path / 'wcpan.drive'
+    return path
+
+
+def get_data_path(root: str):
+    path = pathlib.Path(f'./{root}/local/share')
+    path = path.expanduser()
+    path = path / 'wcpan.drive'
+    return path
+
+
+def get_src_drive():
+    factory = DriveFactory()
+    factory.config_path = get_config_path('src')
+    factory.data_path = get_data_path('src')
+    factory.load_config()
+    return factory
+
+
+def get_dst_drive():
+    factory = DriveFactory()
+    factory.config_path = get_config_path('dst')
+    factory.data_path = get_data_path('dst')
+    factory.load_config()
+    return factory
 
 
 if __name__ == '__main__':
