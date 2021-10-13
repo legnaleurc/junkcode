7c52117d4f6b10607485c73a9419b7a5676e17e0
Wei-Cheng Pan <legnaleurc@gmail.com>
2021-10-13 11:39:48 +0900

    update

--- driveutil/migration/app/progress.py
+++ driveutil/migration/app/progress.py
@@ -5,19 +5,21 @@ import sys
 
 from wcpan.drive.core.drive import Drive, Node
 
-from common import get_src_drive, is_migrated
+from common import get_dst_drive, get_src_drive, is_migrated
 
 
 async def main():
     from_path = sys.argv[1]
-    factory = get_src_drive()
-    async with factory() as drive:
-        root_node = await drive.get_node_by_path(from_path)
+    src_factory = get_src_drive()
+    dst_factory = get_dst_drive()
+    async with src_factory() as src_drive, \
+               dst_factory() as dst_drive:
+        root_node = await src_drive.get_node_by_path(from_path)
         assert root_node is not None
 
-        children = await drive.get_children(root_node)
+        children = await src_drive.get_children(root_node)
         task_list = [
-            calculate_progress(drive, folder)
+            calculate_progress(src_drive, dst_drive, folder)
             for folder in children
         ]
         result_list = await asyncio.gather(*task_list)
@@ -26,14 +28,16 @@ async def main():
             print(f'{folder.name}: {rv * 100:.2f}%')
 
 
-async def calculate_progress(drive: Drive, root_node: Node):
+async def calculate_progress(src_drive: Drive, dst_drive: Drive, root_node: Node):
     total: int = 0
     migrated: int = 0
-    async for root, folders, files in drive.walk(root_node):
-        for file in files:
-            total += file.size
-            if is_migrated(file):
-                migrated += file.size
+    async for root, folders, files in src_drive.walk(root_node):
+        for file_ in files:
+            total += file_.size
+            path = await src_drive.get_path(file_)
+            dst = await dst_drive.get_node_by_path(path)
+            if is_migrated(dst):
+                migrated += file_.size
     return root_node, migrated / total
 
 
