79ba2030ddc6f422fcd5bbf085dd4f9043a83787
Wei-Cheng Pan <legnaleurc@gmail.com>
2022-12-29 10:56:51 +0900

    update

--- sandbox/typescript/clearall/src/lib/consts.ts
+++ sandbox/typescript/clearall/src/lib/consts.ts
@@ -7,4 +7,4 @@ export const CLIENT_SECRET = process.env.CLIENT_SECRET!;
 export const CALLBACK = process.env.CALLBACK!;
 export const TOKEN_FILE = ".token.json";
 export const INTERVAL_MS = 1000 * 60 * 15;
-export const TOKEN_MS = 1000 * 60 * 30;
+export const TOKEN_MS = 1000 * 60 * 60;
