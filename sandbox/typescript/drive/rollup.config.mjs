ffd2857a79c06e16c96e199c6c379d42d2150202
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-10-31 20:56:33 +0900

    update

--- sandbox/typescript/drive/rollup.config.mjs
+++ sandbox/typescript/drive/rollup.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
+import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default defineConfig({
     input: "src/index.ts",
@@ -8,5 +9,6 @@ export default defineConfig({
     },
     plugins: [
         typescript(),
+        nodeResolve(),
     ],
 });
