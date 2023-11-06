1d49dec204f1f9ebd3ae5efca8381bd3f3f55cbb
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-11-06 17:49:28 +0900

    update

--- sandbox/typescript/drive/src/drive.ts
+++ sandbox/typescript/drive/src/drive.ts
@@ -5,10 +5,27 @@ type CreateDriveParameters = {
     createSnapshotService: () => Promise<SnapshotService>;
 };
 export async function createDrive(params: CreateDriveParameters): Promise<Drive> {
-    return new DefaultDrive();
+    const fileService = await params.createFileService();
+    const snapshotService = await params.createSnapshotService();
+    return new DefaultDrive({
+        fileService,
+        snapshotService,
+    });
 }
 
+type DefaultDriveConstructorParameters = {
+    fileService: FileService;
+    snapshotService: SnapshotService;
+};
 class DefaultDrive implements Drive {
+    private fileService: FileService;
+    private snapshotService: SnapshotService;
+
+    constructor(params: DefaultDriveConstructorParameters) {
+        this.fileService = params.fileService;
+        this.snapshotService = params.snapshotService;
+    }
+
     async sync() {}
 
     async * walk() {}
