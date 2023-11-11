d9640c63069a07662ef58d6d44f5e4501dd81d6f
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-11-12 05:06:43 +0900

    update

--- driveutil/faststart/app/main.py
+++ driveutil/faststart/app/main.py
@@ -2,24 +2,20 @@
 
 import argparse
 import asyncio
-import signal
 import sys
-import tempfile
 from concurrent.futures import Executor
-from functools import partial
 from logging import getLogger
 from logging.config import dictConfig
 from tempfile import TemporaryDirectory
-from typing import NoReturn, Any
 
 from wcpan.drive.core.drive import Drive, DriveFactory
 from wcpan.drive.core.types import Node
 from wcpan.drive.core.util import create_executor
 from wcpan.logging import ConfigBuilder
+from wcpan.queue import AioQueue
 
 from .cache import initialize_cache
 from .processor import create_processor, is_oggmedia, is_realmedia
-from .queue_ import consume, produce, NodeGenerator
 
 
 async def main(args: list[str] = None):
@@ -32,62 +28,40 @@ async def main(args: list[str] = None):
     transcode_only: bool = kwargs.transcode_only
     cache_only: bool = kwargs.cache_only
     jobs: int = kwargs.jobs
+    tmp_path: str | None = kwargs.tmp_path
 
     initialize_cache()
     dictConfig(
-        ConfigBuilder(path="./data/migrate.log").add("wcpan.drive", "app").to_dict()
+        ConfigBuilder(path="./data/migrate.log")
+        .add("wcpan")
+        .add("app", level="D")
+        .to_dict()
     )
 
     factory = DriveFactory()
     factory.load_config()
 
-    queue = asyncio.Queue(jobs)
-
-    with create_executor() as pool, TemporaryDirectory() as work_folder:
+    with create_executor() as pool, TemporaryDirectory(
+        dir=tmp_path
+    ) as work_folder, AioQueue.fifo() as queue:
         async with factory(pool=pool) as drive:
             async for change in drive.sync():
                 getLogger(__name__).debug(change)
 
-            work = partial(
-                node_work,
-                drive=drive,
-                work_folder=work_folder,
-                pool=pool,
-                remux_only=remux_only,
-                transcode_only=transcode_only,
-                cache_only=cache_only,
-            )
-            consumer_list = [
-                asyncio.create_task(consume(queue, work), name=f"consumer-{i}")
-                for i in range(jobs)
-            ]
+            async for video_file in walk_root_list(drive, root_path_list):
+                await queue.push(
+                    node_work(
+                        video_file,
+                        drive=drive,
+                        work_folder=work_folder,
+                        pool=pool,
+                        remux_only=remux_only,
+                        transcode_only=transcode_only,
+                        cache_only=cache_only,
+                    )
+                )
 
-            producer = asyncio.create_task(
-                until_finished(
-                    queue,
-                    walk_root_list(drive, root_path_list),
-                    consumer_list,
-                ),
-                name=f"producer",
-            )
-
-            loop = asyncio.get_running_loop()
-            loop.add_signal_handler(
-                signal.SIGINT,
-                partial(
-                    shutdown,
-                    tasks=[
-                        producer,
-                        *consumer_list,
-                    ],
-                ),
-            )
-
-            await asyncio.gather(
-                producer,
-                *consumer_list,
-                return_exceptions=True,
-            )
+            await queue.consume(jobs)
 
     return 0
 
@@ -101,6 +75,7 @@ def parse_args(args: list[str]) -> argparse.Namespace:
     mutex_group.add_argument("--cache-only", action="store_true", default=False)
 
     parser.add_argument("--jobs", "-j", type=int, default=1)
+    parser.add_argument("--tmp-path", type=str)
 
     parser.add_argument("root_path", type=str, nargs="+")
 
@@ -148,28 +123,3 @@ async def node_work(
     )
     if used_quota:
         await asyncio.sleep(60)
-
-
-async def until_finished(
-    queue: asyncio.Queue,
-    node_gen: NodeGenerator,
-    consumer_list: list[asyncio.Task[NoReturn]],
-):
-    await produce(queue, node_gen)
-    await queue.join()
-    cancel_tasks(consumer_list)
-
-
-def shutdown(*, tasks: list[asyncio.Task[Any]]):
-    cancel_tasks(tasks)
-    for task in tasks:
-        if not task.done():
-            task.print_stack()
-        elif task.cancelled():
-            print(f"{task.get_name()} cancelled")
-
-
-def cancel_tasks(tasks: list[asyncio.Task[Any]]):
-    for task in tasks:
-        if not task.done():
-            task.cancel()
