e29564005aff51d8ddb2861d99855a79f3d3a5bd
Wei-Cheng Pan <legnaleurc@gmail.com>
2024-06-13 05:58:28 +0900

    chore: refactor

--- drive/app/jav/_sauce.py
+++ drive/app/jav/_sauce.py
@@ -8,116 +8,135 @@ from bs4 import BeautifulSoup
 from ._types import JavData
 
 
-async def _fetch_from_fanza(session: ClientSession, jav_id: str, query: str) -> str | None:
-    async with session.get(
-        f"https://www.dmm.co.jp/search/=/searchstr={query}/limit=30/sort=rankprofile/"
-    ) as response:
+async def _get_html(session: ClientSession, url: str) -> BeautifulSoup | None:
+    async with session.get(url) as response:
         if response.status != 200:
             return None
 
         html = await response.text(errors="ignore")
-        soup = BeautifulSoup(html, "html.parser")
-        title = soup.select_one(".txt > a:nth-child(1)")
-        if title:
-            title = _normalize_title(title.text)
-            return f"{jav_id} {title}"
-    return None
+
+    return BeautifulSoup(html, "html.parser")
 
 
-async def _fetch_from_mgs(session: ClientSession, jav_id: str, query: str) -> str | None:
-    async with session.get(f"https://www.mgstage.com/product/product_detail/{query}/") as response:
-        if response.status != 200:
-            return None
+async def _fetch_from_fanza(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
+    soup = await _get_html(
+        session,
+        f"https://www.dmm.co.jp/search/=/searchstr={query}/limit=30/sort=rankprofile/",
+    )
+    if not soup:
+        return None
 
-        html = await response.text(errors="ignore")
-        soup = BeautifulSoup(html, "html.parser")
-        title = soup.select_one(".tag")
-        if title:
-            title = _normalize_title(title.text)
-            return f"{jav_id} {title}"
-    return None
+    title = soup.select_one(".txt > a:nth-child(1)")
+    if not title:
+        return None
 
-
-async def _fetch_from_heydouga(session: ClientSession, jav_id: str, query: str) -> str | None:
-    async with session.get(
-        f"https://www.heydouga.com/moviepages/{query}/index.html",
-    ) as response:
-        if response.status != 200:
-            return None
-
-        html = await response.text(errors="ignore")
-        soup = BeautifulSoup(html, "html.parser")
-        title = soup.select_one("#title-bg > h1")
-        if not title:
-            return None
-        for span in title.find_all("span"):
-            span.decompose()
-        return _normalize_title(title.text)
+    title = _normalize_title(title.text)
+    return f"{jav_id} {title}"
 
 
-async def _fetch_from_heyzo(session: ClientSession, jav_id: str, query: str) -> str | None:
-    async with session.get(
-        f"https://www.heyzo.com/moviepages/{query}/index.html",
-    ) as response:
-        if response.status != 200:
-            return None
+async def _fetch_from_mgs(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
+    soup = await _get_html(
+        session, f"https://www.mgstage.com/product/product_detail/{query}/"
+    )
+    if not soup:
+        return None
 
-        html = await response.text(errors="ignore")
-        soup = BeautifulSoup(html, "html.parser")
-        title = soup.select_one("#movie > h1")
-        if not title:
-            return None
-        title = _normalize_title(title.text)
-        title = re.sub(r"\t+", " ", title)
+    title = soup.select_one(".tag")
+    if not title:
+        return None
+
+    title = _normalize_title(title.text)
+    return f"{jav_id} {title}"
+
+
+async def _fetch_from_heydouga(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
+    soup = await _get_html(
+        session, f"https://www.heydouga.com/moviepages/{query}/index.html"
+    )
+    if not soup:
+        return None
+
+    title = soup.select_one("#title-bg > h1")
+    if not title:
+        return None
+
+    for span in title.find_all("span"):
+        span.decompose()
+
+    return _normalize_title(title.text)
+
+
+async def _fetch_from_heyzo(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
+    soup = await _get_html(
+        session, f"https://www.heyzo.com/moviepages/{query}/index.html"
+    )
+    if not soup:
+        return None
+
+    title = soup.select_one("#movie > h1")
+    if not title:
+        return None
+
+    title = _normalize_title(title.text)
+    title = re.sub(r"\t+", " ", title)
+    return f"{jav_id} {title}"
+
+
+async def _fetch_from_carib(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
+    soup = await _get_html(
+        session, f"https://www.caribbeancom.com/moviepages/{query}/index.html"
+    )
+    if not soup:
+        return None
+
+    title = soup.select_one("h1[itemprop=name]")
+    if not title:
+        return None
+    title = _normalize_title(title.text)
+
+    actor = soup.select_one(".movie-spec a[itemprop=actor] > span[itemprop=name]")
+    if not actor:
         return f"{jav_id} {title}"
+    actor = _normalize_title(actor.text)
+
+    return f"{jav_id} {title} {actor}"
 
 
-async def _fetch_from_carib(session: ClientSession, jav_id: str, query: str) -> str | None:
-    async with session.get(
-        f"https://www.caribbeancom.com/moviepages/{query}/index.html",
-    ) as response:
-        if response.status != 200:
-            return None
+async def _fetch_from_caribpr(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
+    soup = await _get_html(
+        session, f"https://www.caribbeancompr.com/moviepages/{query}/index.html"
+    )
+    if not soup:
+        return None
 
-        html = await response.text(errors="ignore")
-        soup = BeautifulSoup(html, "html.parser")
+    title = soup.select_one(".movie-info .heading")
+    if not title:
+        return None
+    title = _normalize_title(title.text)
 
-        title = soup.select_one("h1[itemprop=name]")
-        if not title:
-            return None
-        title = _normalize_title(title.text)
+    actor = soup.select_one(".movie-spec .spec-content > .spec-item")
+    if not actor:
+        return f"{jav_id} {title}"
+    actor = _normalize_title(actor.text)
 
-        actor = soup.select_one(".movie-spec a[itemprop=actor] > span[itemprop=name]")
-        if not actor:
-            return f"{jav_id} {title}"
-        actor = _normalize_title(actor.text)
-
-        return f"{jav_id} {title} {actor}"
-
-
-async def _fetch_from_caribpr(session: ClientSession, jav_id: str, query: str) -> str | None:
-    async with session.get(
-        f"https://www.caribbeancompr.com/moviepages/{query}/index.html",
-    ) as response:
-        if response.status != 200:
-            return None
-
-        html = await response.text(errors="ignore")
-        soup = BeautifulSoup(html, "html.parser")
-
-        title = soup.select_one(".movie-info .heading")
-        if not title:
-            return None
-        title = _normalize_title(title.text)
-
-        actor = soup.select_one(".movie-spec .spec-content > .spec-item")
-        if not actor:
-            return f"{jav_id} {title}"
-
-        return f"{jav_id} {title} {_normalize_title(actor.text)}"
+    return f"{jav_id} {title} {actor}"
 
 
-async def _fetch_from_1pondo(session: ClientSession, jav_id: str, query: str) -> str | None:
+async def _fetch_from_1pondo(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
     async with session.get(
         f"https://www.1pondo.tv/dyn/phpauto/movie_details/movie_id/{query}.json",
     ) as response:
@@ -131,7 +150,9 @@ async def _fetch_from_1pondo(session: ClientSession, jav_id: str, query: str) ->
         return f"{jav_id} {title} {actor}"
 
 
-async def _fetch_from_10musume(session: ClientSession, jav_id: str, query: str) -> str | None:
+async def _fetch_from_10musume(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
     async with session.get(
         f"https://www.10musume.com/dyn/phpauto/movie_details/movie_id/{query}.json",
     ) as response:
@@ -145,7 +166,9 @@ async def _fetch_from_10musume(session: ClientSession, jav_id: str, query: str) 
         return f"10MU {jav_id} {title} {actor}"
 
 
-async def _fetch_from_javbee(session: ClientSession, jav_id: str, query: str) -> str | None:
+async def _fetch_from_javbee(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
     async with session.get(
         "https://javbee.org/search",
         params={
@@ -163,7 +186,9 @@ async def _fetch_from_javbee(session: ClientSession, jav_id: str, query: str) ->
         return _normalize_title(title.text)
 
 
-async def _fetch_from_javtorrent(session: ClientSession, jav_id: str, query: str) -> str | None:
+async def _fetch_from_javtorrent(
+    session: ClientSession, jav_id: str, query: str
+) -> str | None:
     async with session.get(
         "https://jav-torrent.org/search",
         params={
