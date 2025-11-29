8bab2aba97913b759ccce2a4628b49d352cb93da
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-11-29 20:23:35 +0900

    feat: add new script

---
+++ userscript/javbee.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name     JavBee
+// @version  1
+// @grant    none
+// @include  https://*.javbee.vip/*
+// ==/UserScript==
+
+
+function handleImageLinks() {
+  const linkList = document.querySelectorAll('a.img-items');
+  linkList.forEach((a) => {
+    const url = a.textContent.trim();
+    a.href = url;
+  });
+}
+
+
+handleImageLinks();
